536c952bf1138131ec5ec96f2181eab30b76fb5d
agungrizkis <agungsubhan@gmail.com>
2020-04-13 14:14:54 +0700

    rev

--- app/Http/Routes/Web/picking.php
+++ app/Http/Routes/Web/picking.php
@@ -2,4 +2,6 @@
 
 Route::group(['middleware' => 'auth'], function () {
   Route::view('upload-do-for-picking', 'web.picking.upload-do-for-picking.index');
+  Route::view('picking-list', 'web.picking.upload-do-for-picking.index');
+  Route::view('picking-to-lmb', 'web.picking.upload-do-for-picking.index');
 });
