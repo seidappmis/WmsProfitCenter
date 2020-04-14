174edd5d6ac159f2501b12af38a983a1a98c967b
agungrizkis <agungsubhan@gmail.com>
2020-04-14 15:39:45 +0700

    update readme

--- app/Http/View/Composers/AdminNavListComposer.php
+++ app/Http/View/Composers/AdminNavListComposer.php
@@ -15,30 +15,122 @@ class AdminNavListComposer
    */
   public function __construct()
   {
-    $this->menuItems[] = ['name' => 'dashboard', 'label' => 'Dashboard', 'url' => 'dashboard', 'icon' => 'settings_input_svideo'];
+    $this->menuItems[] = ['name' => 'home', 'label' => 'Home', 'url' => 'home', 'icon' => 'settings_input_svideo'];
+
+    $this->menuItems[] = ['name' => 'dashboard', 'label' => 'Dashboard', 'url' => '#', 'icon' => 'play_for_work', 'childs' => [
+      ['name' => 'dashboard', 'label' => 'Graphic Dashboard', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'dashboard', 'label' => 'Graphic Dashboard 2', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'dashboard', 'label' => 'Trucking Monitor', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+    ]];
 
     $this->menuItems[] = ['name' => 'incoming', 'label' => 'Incoming', 'url' => '#', 'icon' => 'play_for_work', 'childs' => [
-      ['name' => 'incoming', 'label' => 'Incoming', 'url' => 'learning-classes', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'incoming', 'label' => 'Finish Good Production', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'incoming', 'label' => 'Incoming Import/OEM', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'incoming', 'label' => 'Confirm Manifest', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'incoming', 'label' => 'Billing Return', 'url' => '#', 'icon' => 'radio_button_unchecked'],
     ]];
-    $this->menuItems[] = ['name' => 'picking', 'label' => 'Picking', 'url' => '#', 'icon' => 'rv_hookup', 'childs' => [
-      ['name' => 'picking', 'label' => 'Upload DO for Picking', 'url' => 'upload-do-for-picking', 'icon' => 'radio_button_unchecked'],
-      ['name' => 'picking', 'label' => 'Picking List', 'url' => 'picking-list', 'icon' => 'radio_button_unchecked'],
-      ['name' => 'picking', 'label' => 'Picking to LMB', 'url' => 'picking-to-lmb', 'icon' => 'radio_button_unchecked'],
+
+    $this->menuItems[] = ['name' => 'other', 'label' => 'Others', 'url' => '#', 'icon' => 'play_for_work', 'childs' => [
+      ['name' => 'other', 'label' => 'Clean Concept', 'url' => '#', 'icon' => 'radio_button_unchecked'],
     ]];
+
+    // $this->menuItems[] = ['name' => 'picking', 'label' => 'Picking', 'url' => '#', 'icon' => 'rv_hookup', 'childs' => [
+    //   ['name' => 'picking', 'label' => 'Upload DO for Picking', 'url' => 'upload-do-for-picking', 'icon' => 'radio_button_unchecked'],
+    //   ['name' => 'picking', 'label' => 'Picking List', 'url' => 'picking-list', 'icon' => 'radio_button_unchecked'],
+    //   ['name' => 'picking', 'label' => 'Picking to LMB', 'url' => 'picking-to-lmb', 'icon' => 'radio_button_unchecked'],
+    // ]];
     $this->menuItems[] = ['name' => 'outgoing', 'label' => 'Outgoing', 'url' => '#', 'icon' => 'present_to_all', 'childs' => [
-      ['name' => 'outgoing', 'label' => 'Outgoing', 'url' => 'learning-classes', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Upload Concept', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'IDCard Scan', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Assign Vehicles', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Select Gate', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Loading Process', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Complete', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Manifest Regular', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Manifest AS', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Update Manifest', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'outgoing', 'label' => 'Overload Concept or DO', 'url' => '#', 'icon' => 'radio_button_unchecked'],
     ]];
+
     $this->menuItems[] = ['name' => 'invoicing', 'label' => 'Invoicing', 'url' => '#', 'icon' => 'pages', 'childs' => [
-      ['name' => 'invoicing', 'label' => 'Invoicing', 'url' => 'learning-classes', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'invoicing', 'label' => 'List of Unconfirm DO', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'invoicing', 'label' => 'Receipt Invoice', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'invoicing', 'label' => 'Receipt Invoice Accounting', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'invoicing', 'label' => 'Summary Freight Cost Analysis', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'invoicing', 'label' => 'Branch Invoicing', 'url' => '#', 'icon' => 'radio_button_unchecked'],
     ]];
     $this->menuItems[] = ['name' => 'inventory', 'label' => 'Inventory', 'url' => '#', 'icon' => 'store', 'childs' => [
-      ['name' => 'inventory', 'label' => 'Inventory', 'url' => 'learning-classes', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'inventory', 'label' => 'Storage Inventory Monitoring', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'inventory', 'label' => 'Upload Inventory Storage', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'inventory', 'label' => 'Adjust Inventory Movement', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'inventory', 'label' => 'Transfer SLoc', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'inventory', 'label' => 'Cancel Movement', 'url' => '#', 'icon' => 'radio_button_unchecked'],
     ]];
+
+    $this->menuItems[] = ['name' => 'report', 'label' => 'Return', 'url' => '#', 'icon' => 'rate_review', 'childs' => [
+      ['name' => 'report', 'label' => 'Task Notice', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+    ]];
+
+    $this->menuItems[] = ['name' => 'report', 'label' => 'Stock Take', 'url' => '#', 'icon' => 'rate_review', 'childs' => [
+      ['name' => 'report', 'label' => 'Stock Take Schedule', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Stock Take Create Tag', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Stock Take Input 1', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Stock Take Input 2', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Stock Take Quick Count', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Stock Take Compare SAP', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+    ]];
+
     $this->menuItems[] = ['name' => 'report', 'label' => 'Report', 'url' => '#', 'icon' => 'rate_review', 'childs' => [
-      ['name' => 'report', 'label' => 'Report', 'url' => 'learning-classes', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Master', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Master User', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Standby Driver List', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Concept or DO Outstanding List', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Loading Status List', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Concept Coming VS Actual Loading', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Concept Issue', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Loading Lead Time', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Loading Summary', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report KPi Expeditions', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Summary Incoming Report', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Summary Outgoing Report', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Master Freight Cost', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Summary Freight Cost Report Per Manifest', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Summary Freight Cost Report Per Region', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Overload Concept or DO', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Summary Task Notice', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report User Mobile', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Summary LMB Report', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Inventory Movement', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Stock Inventory', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Serial Number Trace', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Report Occupancy', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Summary WH Transporter Report', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'report', 'label' => 'Summary DO Confirmed', 'url' => '#', 'icon' => 'radio_button_unchecked'],
     ]];
     $this->menuItems[] = ['name' => 'master', 'label' => 'Master Data', 'url' => '#', 'icon' => 'storage', 'childs' => [
-      ['name' => 'master', 'label' => 'Master Data', 'url' => 'learning-classes', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Gate', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Destioantion', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Vehicle', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Expedition', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Vehicle Expedition', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Driver', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Destionation City', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Freight Cost', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Storage Master', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Model', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Vendor', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Model Exception', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Master Branch Expedition', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Branch Expedition Vehicle', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Branch Master Driver', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'master', 'label' => 'Destination City of Branch', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+    ]];
+    $this->menuItems[] = ['name' => 'setting', 'label' => 'Setting', 'url' => '#', 'icon' => 'storage', 'childs' => [
+      ['name' => 'setting', 'label' => 'User Manager', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'setting', 'label' => 'User Roles', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'setting', 'label' => 'Master Area', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'setting', 'label' => 'Master Cabang', 'url' => '#', 'icon' => 'radio_button_unchecked'],
+      ['name' => 'setting', 'label' => 'Master User Mobile', 'url' => '#', 'icon' => 'radio_button_unchecked'],
     ]];
   }
 
---
+++ resources/views/web/picking/picking-to-lmb/index.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.materialize.index')
+{{-- @include('admin.materi.modal_form_materi') --}}
+
+@section('content')
+<div class="row">
+
+    @component('layouts.materialize.components.title-wrapper')
+        <div class="row">
+            <div class="col s12 m8 l8">
+                <h5 class="breadcrumbs-title mt-0 mb-0"><span>Picking to LMB</span></h5>
+                <ol class="breadcrumbs mb-0">
+                    <li class="breadcrumb-item"><a href="{{ url('/') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Picking to LMB</li>
+                </ol>
+            </div>
+            <div class="col s12 m4 l4">
+                <!---- Search ----->
+                <div class="app-wrapper">
+                  <div class="datatable-search">
+                    <i class="material-icons mr-2 search-icon">search</i>
+                    <input type="text" placeholder="Search" class="app-filter" id="global_filter">
+                  </div>
+                </div>
+            </div>
+        </div>
+    @endcomponent
+    
+    <div class="col s12">
+        <div class="container">
+            <div class="section">
+                <div class="card">
+                    <div class="card-content p-0">
+                        <div class="section-data-tables"> 
+                          <table id="data-table-section-contents" class="display" width="100%">
+                              <thead>
+                                  <tr>
+                                    <th data-priority="1" width="30px">No.</th>
+                                    <th>DELIVERY NO.</th>
+                                    <th>DELIVERY ITEM</th>
+                                    <th>DO DATE</th>
+                                    <th>CUSTOMER CODE</th>
+                                    <th>CUSTOMER NAME</th>
+                                    <th>MODEL</th>
+                                    <th>EAN CODE</th>
+                                    <th>QUANTITY</th>
+                                    <th width="50px;"></th>
+                                  </tr>
+                              </thead>
+                              <tbody></tbody>
+                          </table>
+                        </div>
+                        <!-- datatable ends -->
+                    </div>
+                </div>
+            </div>
+            <!---- Button Add ----->
+            <div style="bottom: 50px; right: 19px;" class="fixed-action-btn direction-top"><a href="#" class="btn-floating indigo darken-2 gradient-shadow modal-trigger"><i class="material-icons">add</i></a>
+            </div>
+        </div>
+        <div class="content-overlay"></div>
+    </div>
+</div>
+@endsection
+
+@push('script_js')
+<script type="text/javascript">
+    var dtdatatable = $('#data-table-section-contents').DataTable({
+        serverSide: false,
+        scrollX: true,
+        responsive: true,
+        ajax: {
+            url: '/',
+            type: 'GET',
+            data: function(d) {
+                d.search['value'] = $('#global_filter').val()
+              }
+        },
+        order: [1, 'asc'],
+        // columns: [
+        //     {data: 'DT_RowIndex', orderable:false, searchable: false, className: 'center-align'},
+        //     {data: 'content_title', name: 'content_title', className: 'detail'},
+        //     {data: 'video', name: 'video', className: 'detail', orderable: false, searchable: false},
+        //     {data: 'summary_title', name: 'summary_title', className: 'detail'},
+        //     {data: 'question_package_id', name: 'question_package_id', className: 'detail'},
+        //     {data: 'action', className: 'center-align'},
+        // ]
+    });
+
+    dtdatatable.on('click', '.btn-edit', function(event) {
+      var id = $(this).data('id');
+      window.location.href = '' ;
+    });
+
+    dtdatatable.on('click', '.btn-delete', function(event) {
+      var id = $(this).data('id');
+      event.preventDefault();
+      /* Act on the event */
+      // Ditanyain dulu usernya mau beneran delete data nya nggak.
+      swal({
+        title: "Are you sure?",
+        text: "You will not be able to recover this imaginary file!",
+        icon: 'warning',
+        buttons: {
+          cancel: true,
+          delete: 'Yes, Delete It'
+        }
+      }).then(function (confirm) { // proses confirm
+        if (confirm) { // Bila oke post ajax ke url delete nya
+          // Ajax Post Delete
+          $.ajax({
+            url: id,
+            type: 'DELETE',
+          })
+          .done(function() { // Kalau ajax nya success
+            swal("Good job!", "You clicked the button!", "success") // alert success
+            dtdatatable.ajax.reload(null, false); // reload datatable
+          })
+          .fail(function() { // Kalau ajax nya gagal
+            console.log("error");
+          });
+          
+        }
+      })
+    });
+</script>
+@endpush
