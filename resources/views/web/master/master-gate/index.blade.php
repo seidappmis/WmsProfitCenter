c3834a27ffeaaa116774b2d39a7c5f9b81739088
daniswarags <sukmagaluh@gmail.com>
2020-04-15 14:51:00 +0700

    rev

--- resources/views/web/master/master-gate/index.blade.php
+++ resources/views/web/master/master-gate/index.blade.php
@@ -47,7 +47,18 @@
                                     <th width="50px;"></th>
                                   </tr>
                               </thead>
-                              <tbody></tbody>
+                              <tbody>
+                                <tr>
+                                  <th data-priority="1" width="30px">1.</th>
+                                  <th>101</th>
+                                  <th >GATE NO. 1-A KARAWANG WAREHOUSE</th>
+                                  <th width="400px">KARAWANG</th>
+                                  <th width="50px;">
+                                    <span class="waves-effect btn-floating btn-small amber darken-4 btn-edit" data-id=""><i class="material-icons">edit</i></span>
+                                    <span class="waves-effect btn-floating red darken-4 btn-small btn-delete" data-id=""><i class="material-icons">delete</i></span>
+                                  </th>
+                                </tr>
+                              </tbody>
                           </table>
                         </div>
                         <!-- datatable ends -->
@@ -62,7 +73,7 @@
 @endsection
 
 @push('page-modal')
-<!-- Modal Structure -->
+<!-- Modal Structure Add-->
 <div id="modal-add" class="modal">
   <form id="form-kelas" onsubmit="return false;">
   <div class="modal-content">
@@ -96,16 +107,36 @@
   </form> 
 </div>
 
-<!-- Modal Structure -->
+<!-- Modal Structure Edit-->
 <div id="modal-edit" class="modal">
-  <form id="form-kelas-edit" onsubmit="return false;">
+  <form id="form-edit" onsubmit="return false;">
   <div class="modal-content">
-    <h4>Edit Kelas</h4>
-    
+    <h4>Edit Gate</h4>
+    @csrf
+    <input type="hidden" name="id">
+    <div class="row">
+      <div class="input-field col s12">
+        <input id="number" type="text" class="validate" name="gate_number" required>
+        <label for="number">Gate Number</label>
+      </div>
+      <div class="input-field col s12">
+        <input id="description" type="text" class="validate" name="description" required>
+        <label for="description">Description</label>
+      </div>
+      <div class="input-field col m6 s12">
+        <select>
+            <option value="" disabled selected>-- Select --</option>
+            <option value="1">KARAWANG</option>
+            <option value="2">SURABAYA HUB</option>
+            <option value="3">SWADAYA</option>
+        </select>
+        <label>Area</label>
+      </div>
+    </div>
   </div>
   <div class="modal-footer">
-    <span class="modal-action modal-close waves-effect waves-green btn-flat">Save</span>
-    <button type="submit" class="modal-action waves-effect waves-green btn-flat">Cancel</button>
+    <button type="submit" class="modal-action waves-effect waves-green btn-flat">Update</button>
+    <span class="modal-action modal-close waves-effect waves-green btn-flat">Cancel</span>
   </div>
   </form>
 </div>
