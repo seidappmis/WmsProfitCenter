26566f5b49f5912c8cc500802f81b92263264461
daniswarags <sukmagaluh@gmail.com>
2020-04-15 14:36:43 +0700

    rev

--- resources/views/web/master/master-gate/index.blade.php
+++ resources/views/web/master/master-gate/index.blade.php
@@ -21,7 +21,9 @@
                     <input type="text" placeholder="Search" class="app-filter" id="global_filter">
                   </div>
                 </div>
-                <button class="btn btn-large waves-effect waves-light btn-add" type="submit" name="action">New Gate</button>
+                <!---- Button Modal Add ----->
+                <a class="btn btn-large waves-effect waves-light btn-add modal-trigger" href="#modal-add">New Gate</a>
+                <!-- <button class="btn btn-large waves-effect waves-light btn-add" type="submit" name="action">New Gate</button> -->
               </div>
             </div>
             <div class="col s12 m3">
@@ -58,6 +60,56 @@
     </div>
 </div>
 @endsection
+
+@push('page-modal')
+<!-- Modal Structure -->
+<div id="modal-add" class="modal">
+  <form id="form-kelas" onsubmit="return false;">
+  <div class="modal-content">
+    <h4>New Gate</h4>
+    @csrf
+    <input type="hidden" name="id">
+    <div class="row">
+      <div class="input-field col s12">
+        <input id="number" type="text" class="validate" name="gate_number" required>
+        <label for="number">Gate Number</label>
+      </div>
+      <div class="input-field col s12">
+        <input id="description" type="text" class="validate" name="description" required>
+        <label for="description">Description</label>
+      </div>
+      <div class="input-field col m6 s12">
+        <select>
+            <option value="" disabled selected>-- Select --</option>
+            <option value="1">KARAWANG</option>
+            <option value="2">SURABAYA HUB</option>
+            <option value="3">SWADAYA</option>
+        </select>
+        <label>Area</label>
+      </div>
+    </div>
+  </div>
+  <div class="modal-footer">
+    <button type="submit" class="modal-action waves-effect waves-green btn-flat">Save</button>
+    <span class="modal-action modal-close waves-effect waves-green btn-flat">Cancel</span>
+  </div>
+  </form> 
+</div>
+
+<!-- Modal Structure -->
+<div id="modal-edit" class="modal">
+  <form id="form-kelas-edit" onsubmit="return false;">
+  <div class="modal-content">
+    <h4>Edit Kelas</h4>
+    
+  </div>
+  <div class="modal-footer">
+    <span class="modal-action modal-close waves-effect waves-green btn-flat">Save</span>
+    <button type="submit" class="modal-action waves-effect waves-green btn-flat">Cancel</button>
+  </div>
+  </form>
+</div>
+@endpush
 
 @push('script_js')
 <script type="text/javascript">
