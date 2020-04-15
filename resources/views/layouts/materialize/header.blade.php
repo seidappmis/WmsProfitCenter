eb1a308accb1370cff63022879a488ef5a8fce6e
agungrizkis <agungsubhan@gmail.com>
2020-04-16 01:27:50 +0700

    rev

--- resources/views/layouts/materialize/header.blade.php
+++ resources/views/layouts/materialize/header.blade.php
@@ -3,16 +3,21 @@
   <div class="navbar navbar-fixed"> 
     <nav class="navbar-main navbar-color nav-collapsible sideNav-lock navbar-dark no-shadow">
       <div class="nav-wrapper">
-        <div class="header-search-wrapper hide-on-med-and-down"><i class="material-icons">search</i>
+        {{-- <div class="header-search-wrapper hide-on-med-and-down"><i class="material-icons">search</i>
           <input class="header-search-input z-depth-2" type="text" name="Search" placeholder="Cari">
-        </div>
+        </div> --}}
         <ul class="navbar-list right">
-          <li class="hide-on-large-only"><a class="waves-effect waves-block waves-light search-button" href="javascript:void(0);"><i class="material-icons">search</i></a></li>
-          <li><a class="waves-effect waves-block waves-light notification-button" href="javascript:void(0);" data-target="notifications-dropdown"><i class="material-icons">notifications_none<small class="notification-badge">5</small></i></a></li>
-          <li><a class="waves-effect waves-block waves-light profile-button" href="javascript:void(0);" data-target="profile-dropdown"><span class="avatar-status avatar-online"><img src="{{ url('materialize/images/avatar/avatar-7.png') }}" alt="avatar"><i></i></span></a></li>
+          {{-- <li class="hide-on-large-only"><a class="waves-effect waves-block waves-light search-button" href="javascript:void(0);"><i class="material-icons">search</i></a></li> --}}
+          {{-- <li><a class="waves-effect waves-block waves-light notification-button" href="javascript:void(0);" data-target="notifications-dropdown"><i class="material-icons">notifications_none<small class="notification-badge">5</small></i></a></li> --}}
+          <li>
+            <a class="waves-effect waves-block waves-light profile-button" href="javascript:void(0);" data-target="profile-dropdown">
+              Administrator | PT. SEID HQ JAKARTA
+              <span class=""><i class="material-icons" style="vertical-align: middle;">arrow_drop_down</i></span>
+            </a>
+          </li>
         </ul>
         <!-- notifications-dropdown-->
-        <ul class="dropdown-content" id="notifications-dropdown">
+        {{-- <ul class="dropdown-content" id="notifications-dropdown">
           <li>
             <h6>NOTIFICATIONS<span class="new badge">5</span></h6>
           </li>
@@ -32,11 +37,11 @@
           <li><a class="grey-text text-darken-2" href="#!"><span class="material-icons icon-bg-circle amber small">trending_up</span> Generate monthly report</a>
             <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">1 week ago</time>
           </li>
-        </ul>
+        </ul> --}}
         <!-- profile-dropdown-->
         <ul class="dropdown-content" id="profile-dropdown">
-          <li><a class="grey-text text-darken-1" href="{{ url('profile') }}"><i class="material-icons">person_outline</i> Profil</a></li>
-          <li><a class="grey-text text-darken-1" href="#"><i class="material-icons">vpn_key</i> Ganti Password</a></li>
+          {{-- <li><a class="grey-text text-darken-1" href="{{ url('profile') }}"><i class="material-icons">person_outline</i> Profil</a></li> --}}
+          <li><a class="grey-text text-darken-1" href="#"><i class="material-icons">vpn_key</i> Change Password</a></li>
           <li class="divider"></li>
           <!-- <li><a class="grey-text text-darken-1" href="user-lock-screen.html"><i class="material-icons">lock_outline</i> Lock</a></li> -->
           <li><a class="grey-text text-darken-1" href="{{ url('logout') }}" onclick="event.preventDefault();
