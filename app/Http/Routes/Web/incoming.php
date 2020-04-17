f45eb61e4ce783aeb94d864871b4893c68e691d3
daniswarags <sukmagaluh@gmail.com>
2020-04-17 23:14:51 +0700

    mockup incoming

--- app/Http/Routes/Web/incoming.php
+++ app/Http/Routes/Web/incoming.php
@@ -2,6 +2,8 @@
 
 Route::group(['middleware' => 'auth'], function () {
   Route::view('finish-good-production', 'web.incoming.finish-good-production.index');
+  Route::view('finish-good-production/create', 'web.incoming.finish-good-production.create');
+
   Route::view('incoming-import-oem', 'web.incoming.incoming-import-oem.index');
   Route::view('confirm-manifest', 'web.incoming.confirm-manifest.index');
   Route::view('billing-return', 'web.incoming.billing-return.index');
---
+++ resources/views/web/incoming/finish-good-production/create.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.materialize.index')
+
+@section('content')
+<div class="row">
+
+    @component('layouts.materialize.components.title-wrapper')
+        <div class="row">
+            <div class="col s12 m6">
+                <h5 class="breadcrumbs-title mt-0 mb-0"><span>Finish Good Production</span></h5>
+                <ol class="breadcrumbs mb-0">
+                    <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                    <li class="breadcrumb-item active">Finish Good Production</li>
+                </ol>
+            </div>
+            <div class="col s12 m2"></div>
+            <div class="col s12 m4">
+              <div class="display-flex">
+                <!---- Search ----->
+                <div class="app-wrapper mr-2">
+                  <div class="datatable-search">
+                    <select>
+				        <option value="" disabled>-- Select Area --</option>
+				        <option value="1" selected>KARAWANG</option>
+				        <option value="2">SURABAYA HUB</option>
+				        <option value="3">SWADAYA</option>
+				    </select>
+				    <!-- <label>Area</label> -->
+                  </div>
+                </div>
+                <!---- Button Back ----->
+                <a class="btn btn-large waves-effect waves-light indigo" href="{{ url('finish-good-production') }}">Back</a>
+              </div>
+            </div>
+        </div>
+    @endcomponent
+    
+    <div class="col s12">
+        <div class="container">
+            <div class="section">
+                <div class="card">
+                    <div class="card-content">
+                    	<div class="row mb-2">
+                    	  <div class="input-field col s12">
+                    	  	<!-- <input id="number" type="text" class="validate" name="gate_number" required> -->
+							<label for="no">Receipt No. :</label>
+                    	  </div>
+                    	  <div class="input-field col s12">
+                    	  	<!-- <input id="number" type="text" class="validate" name="gate_number" required> -->
+							<label for="no">Ticket No. :</label>
+                    	  </div>
+                    	  <div class="input-field col s12">
+                    	  	<!-- <input id="number" type="text" class="validate" name="gate_number" required> -->
+							<label for="no">Warehouse :</label>
+                    	  </div>
+                    	  <div class="input-field col s12">
+                    	  	<!-- <input id="number" type="text" class="validate" name="gate_number" required> -->
+							<label for="no">Factory :</label>
+                    	  </div>
+                    	</div>
+                    	<br>
+                    	<div class="row mb-2">
+                    	  <div class="col s12">
+                    		<h4 class="card-title">List Barcode Detail from Factory</h4>
+                    		<div class="card-content col s12">
+                    		  <p>No Results Found</p>
+                    	  	</div>
+                    	  </div>
+                    	</div>
+                    	<br>
+                    	<div class="row">
+                    	  <div class="col s12">
+                    		<h4 class="card-title">Find Delivery Ticket</h4>
+                    	  </div>
+                    	  <div class="input-field col s12">
+							<select>
+						        <option value="" disabled selected>-- Select Plant--</option>
+						        <option value="1">ALL PLANT</option>
+						        <option value="2">HA</option>
+						        <option value="3">TV</option>
+						    </select>
+						    <label for="number">Choose Plant</label>
+						  </div>
+						  <div class="input-field col s12">
+							<input id="delivery" type="text" class="validate" name="delivery" required>
+						    <label for="delivery">Delivery Ticket</label>
+						  </div>
+						  <div class="input-field col s12 m6">
+							<select>
+						        <option value="" disabled>-- Select Type--</option>
+						        <option value="1" selected>Local</option>
+						        <!-- <option value="2">HA</option>
+						        <option value="3">TV</option> -->
+						    </select>
+						    <label for="number">Choose Type</label>
+						  </div>
+						  <div class="input-field col s12 m6">
+						  	<button type="submit" class="waves-effect waves-light indigo btn-small btn">Search Delivery Ticket</button>
+						  </div>
+                    	</div>
+                    	<form>
+                    	  <div class="row">
+	                    	<div class="col s12">
+	                    	  <h4 class="card-title">Assign Delivery Ticket</h4>
+	                    	</div>
+	                      </div>
+	                      <div class="card-content">
+	                      <div class="row">
+	                      	<div class="col s12 m4 l5">
+	                      		<p>s12 m4</p>
+	                      	</div>
+						    <div class="col s12 m4 l2">
+						    	<button type="submit" class="waves-effect waves-light indigo btn">>></button>
+						    	<br>
+						    	<button type="submit" class="waves-effect waves-light indigo btn"><<</button>
+						    </div>
+						    <div class="col s12 m4 l5"><p>s12 m4</p></div>
+	                      </div>
+	                  	  </div>
+						</form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-overlay"></div>
+    </div>
+</div>
+@endsection
+
+@push('script_js')
+<script type="text/javascript">
+ 	$('.collapsible').collapsible({
+        accordion:true
+    });
+</script>
+@endpush
