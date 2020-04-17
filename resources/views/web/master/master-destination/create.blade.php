57816ebb439ea6659a03530d7aa2900a27e4ddda
daniswarags <sukmagaluh@gmail.com>
2020-04-17 09:21:38 +0700

    rev

--- resources/views/web/master/master-destination/create.blade.php
+++ resources/views/web/master/master-destination/create.blade.php
@@ -18,50 +18,53 @@
     <div class="col s12">
         <div class="container">
             <div class="section">
-                <!-- <div class="card"> -->
-                    <div class="card-content p-0">
-                        <ul class="collapsible">
-						   <li class="active">
-							   <div class="collapsible-header">New Destination</div>
-							   <div class="collapsible-body white">
-							   		<div class="row">
-									  <div class="input-field col s12">
-										<input id="number" type="text" class="validate" name="number" required>
-									    <label for="number">Destination Number</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="description" type="text" class="validate" name="description" required>
-									    <label for="description">Description</label>
-									  </div>
-                                      <div class="input-field col s12">
-                                        <p>
-                                          <label for="region">
-                                            <span>Region</span>
-                                          </label>
-                                          <label>
-                                            <input class="with-gap" name="group1" type="radio" checked/>
-                                            <span>New Region</span>
-                                          </label>
-                                          <label>
-                                            <input class="with-gap" name="group1" type="radio" />
-                                            <span>Current</span>
-                                          </label>
-                                        </p>
-                                      </div>
-									  <div class="input-field col s12">
-                                        <input id="cabang" type="text" class="validate" name="cabang" required>
-                                        <label for="cabang">Cabang</label>
-                                      </div>
-									</div>
-									<div class="row">
-									  <button type="submit" class="waves-effect waves-light indigo btn">Save</button>
-									  <a class="waves-effect waves-light btn" href="{{ url('master-destination') }}">Cancel</a>
-									</div>
-							   </div>
-						   </li>
-						</ul>
+                <div class="card">
+                    <div class="card-content">
+                        <h4 class="card-title">New Destination</h4>
+                        <form>
+					   		<div class="row">
+							  <div class="input-field col s12">
+								<input id="number" type="text" class="validate" name="number" required>
+							    <label for="number">Destination Number</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="description" type="text" class="validate" name="description" required>
+							    <label for="description">Description</label>
+							  </div>
+                              <div class="row">
+                              <div class="input-field col s12 p-1">
+                                <label for="region">
+                                    <span>Region</span>
+                                </label>
+                              </div>
+                              <div class="input-field col 12">
+                                <p>
+                                  <label>
+                                    <input class="with-gap" name="group1" type="radio" checked/>
+                                    <span>New Region</span>
+                                  </label>
+                                  <label>
+                                    <input class="with-gap" name="group1" type="radio" />
+                                    <span>Current</span>
+                                  </label>
+                                </p>
+                              </div>
+                              </div>
+							  <div class="input-field col s12">
+                                <input id="cabang" type="text" class="validate" name="cabang" required>
+                                <label for="cabang">Cabang</label>
+                              </div>
+							</div>
+                            <br>
+							<div class="row">
+                              <div class="input-field col s12">
+    							  <button type="submit" class="waves-effect waves-light indigo btn">Save</button>
+    							  <a class="waves-effect waves-light btn" href="{{ url('master-destination') }}">Cancel</a>
+                              </div>
+							</div>
+					    </form>
                     </div>
-                <!-- </div> -->
+                </div>
             </div>
         </div>
         <div class="content-overlay"></div>
--- resources/views/web/master/master-expedition/create.blade.php
+++ resources/views/web/master/master-expedition/create.blade.php
@@ -18,75 +18,74 @@
     <div class="col s12">
         <div class="container">
             <div class="section">
-                <!-- <div class="card"> -->
-                    <div class="card-content p-0">
-                        <ul class="collapsible">
-						   <li class="active">
-							   <div class="collapsible-header">New Expedition</div>
-							   <div class="collapsible-body white">
-							   		<div class="row">
-									  <div class="input-field col s12">
-										<input id="code" type="text" class="validate" name="code" required>
-									    <label for="code">Code</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="name" type="text" class="validate" name="name" required>
-									    <label for="name">Expedition Name</label>
-									  </div>
-									  <div class="input-field col s12">
-								        <textarea id="address" class="materialize-textarea"></textarea>
-								        <label for="address">Address</label>
-								      </div>
-								      <div class="input-field col s12">
-									    <input id="sapcode" type="text" class="validate" name="sapcode" required>
-									    <label for="sapcode">SAP CODE</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="npwp" type="text" class="validate" name="npwp">
-									    <label for="npwp">NPWP</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="cp" type="text" class="validate" name="cp">
-									    <label for="cp">CONTACT PERSON</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="phone1" type="number" class="validate" name="phone1">
-									    <label for="phone1">PHONE NUMBER 1</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="phone2" type="number" class="validate" name="phone2">
-									    <label for="phone2">PHONE NUMBER 2</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="fax" type="number" class="validate" name="fax">
-									    <label for="fax">FAX NUMBER</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="bank" type="text" class="validate" name="bank">
-									    <label for="bank">BANK</label>
-									  </div>
-									  <div class="input-field col s12">
-									    <input id="currency" type="text" class="validate" name="currency">
-									    <label for="currency">CURRENCY</label>
-									  </div>
-									  <div class="input-field col m6 s12">
-									    <p>
-									      <label>
-									        <input type="checkbox" class="filled-in" checked="checked" />
-									        <span>ACTIVE</span>
-									      </label>
-									    </p>
-									  </div>
-									</div>
-									<div class="row">
-									  <button type="submit" class="waves-effect waves-light indigo btn">Save</button>
-									  <a class="waves-effect waves-light btn" href="{{ url('master-expedition') }}">Cancel</a>
-									</div>
-							   </div>
-						   </li>
-						</ul>
+                <div class="card">
+                    <div class="card-content">
+                    	<h4 class="card-title">New Expedition</h4>
+                        <form>
+					   		<div class="row">
+							  <div class="input-field col s12">
+								<input id="code" type="text" class="validate" name="code" required>
+							    <label for="code">Code</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="name" type="text" class="validate" name="name" required>
+							    <label for="name">Expedition Name</label>
+							  </div>
+							  <div class="input-field col s12">
+						        <textarea id="address" class="materialize-textarea"></textarea>
+						        <label for="address">Address</label>
+						      </div>
+						      <div class="input-field col s12">
+							    <input id="sapcode" type="text" class="validate" name="sapcode" required>
+							    <label for="sapcode">SAP CODE</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="npwp" type="text" class="validate" name="npwp">
+							    <label for="npwp">NPWP</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="cp" type="text" class="validate" name="cp">
+							    <label for="cp">CONTACT PERSON</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="phone1" type="number" class="validate" name="phone1">
+							    <label for="phone1">PHONE NUMBER 1</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="phone2" type="number" class="validate" name="phone2">
+							    <label for="phone2">PHONE NUMBER 2</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="fax" type="number" class="validate" name="fax">
+							    <label for="fax">FAX NUMBER</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="bank" type="text" class="validate" name="bank">
+							    <label for="bank">BANK</label>
+							  </div>
+							  <div class="input-field col s12">
+							    <input id="currency" type="text" class="validate" name="currency">
+							    <label for="currency">CURRENCY</label>
+							  </div>
+							  <div class="input-field col m6 s12">
+							    <p>
+							      <label>
+							        <input type="checkbox" class="filled-in" checked="checked" />
+							        <span>ACTIVE</span>
+							      </label>
+							    </p>
+							  </div>
+							</div>
+							<br>
+							<div class="row">
+							  <div class="input-field col m6 s12">
+								  <button type="submit" class="waves-effect waves-light indigo btn">Save</button>
+								  <a class="waves-effect waves-light btn" href="{{ url('master-expedition') }}">Cancel</a>
+							  </div>
+							</div>
+						</form>
                     </div>
-                <!-- </div> -->
+                </div>
             </div>
         </div>
         <div class="content-overlay"></div>
