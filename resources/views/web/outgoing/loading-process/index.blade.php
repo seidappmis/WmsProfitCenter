dc98ed1ba649c6db04f827ac34e53afd807c9690
agungrizkis <agungsubhan@gmail.com>
2020-04-17 13:27:09 +0700

    rev

--- resources/views/web/outgoing/loading-process/index.blade.php
+++ resources/views/web/outgoing/loading-process/index.blade.php
@@ -6,10 +6,10 @@
   @component('layouts.materialize.components.title-wrapper')
       <div class="row">
           <div class="col s12 m6">
-              <h5 class="breadcrumbs-title mt-0 mb-0"><span>Stock Take Schedule</span></h5>
+              <h5 class="breadcrumbs-title mt-0 mb-0"><span>Loading Process</span></h5>
               <ol class="breadcrumbs mb-0">
                   <li class="breadcrumb-item"><a href="{{ url('/') }}">Dashboard</a></li>
-                  <li class="breadcrumb-item active">Stock Take Schedule</li>
+                  <li class="breadcrumb-item active">Loading Process</li>
               </ol>
           </div>
           <div class="col s12 m2">
