6e81f453bd9b386c6e40e73f43acb5c28536d502
daniswarags <sukmagaluh@gmail.com>
2020-04-17 11:05:09 +0700

    rev

---
+++ resources/views/web/master/master-model/_form.blade.php
@@ -0,0 +1,21 @@
+<form>
+  <div class="row">
+	<div class="input-field">
+	  <div class="col s12 m4 l3">
+	    <p>Data File</p>
+	  </div>
+	  <div class="col s12 m8 l9">
+	    <input type="file" required id="input-file-now" class="dropify" name="file" data-default-file="" data-height="100"/>
+	    <p>Format File : .csv</p>
+	  </div>
+   	</div>
+   	<div class="input-field">
+	  <div class="col s12 m4 l3">
+	    <p>Format Layout Column :</p>
+	  </div>
+	  <div class="col s12 m8 l9">
+	    <p>[MODEL NAME],[EANCODE],[CBM UNIT],[MATERIAL GROUP CODE],[CATEGORY],[MODEL_TYPE],[PCS/CTN],[CTN/PLT],[MAX PALET],[DESCRIPTION],[PRICE1],[PRICE2],[PRICE3]</p>
+	  </div>
+   	</div>
+  </div>
+</form>
---
+++ resources/views/web/master/master-model/modal-form.blade.php
@@ -0,0 +1,22 @@
+@push('page-modal')
+<!---- Modal Upload ----->
+<div id="modal-upload" class="modal">
+  <form id="form-menu" onsubmit="return false;" enctype="multipart/form-data">
+  <div class="modal-content">
+    <h4>Upload Model</h4>
+    @include('web.master.master-model._form')
+  </div>
+  <div class="modal-footer">
+    <button type="submit" class="modal-action waves-effect waves-green btn-flat">Upload</button>
+    <span class="modal-action modal-close waves-effect waves-green btn-flat">Cancel</span>
+  </div>
+  </form>
+</div>
+@endpush
+
+@push('script_js')
+<script type="text/javascript">
+    //Upload File
+    $('.dropify').dropify();
+</script>
+@endpush
