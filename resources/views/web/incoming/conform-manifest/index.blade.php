01b85e10a3dec2b8e8048f1a0e104ee84f6031c2
agungrizkis <agungsubhan@gmail.com>
2020-04-21 16:27:44 +0700

    revisi search comform manifest

--- resources/views/web/incoming/conform-manifest/index.blade.php
+++ resources/views/web/incoming/conform-manifest/index.blade.php
@@ -25,6 +25,16 @@
                         <li class="active">
                           <div class="collapsible-header"><i class="material-icons">filter_drama</i>From Manifest HQ</div>
                           <div class="collapsible-body p-0">
+                            <div class="row">
+                              <div class="col s12 offset-m6 m6">
+                                <div class="app-wrapper ml-2 mt-2 mr-2">
+                                  <div class="datatable-search mb-0">
+                                    <i class="material-icons mr-2 search-icon">search</i>
+                                    <input type="text" placeholder="Search" class="app-filter" id="global_filter">
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
                             <div class="section-data-tables"> 
                               <table id="data-table-1" class="display" width="100%">
                                   <thead>
@@ -66,6 +76,16 @@
                         <li class="active">
                           <div class="collapsible-header"><i class="material-icons">filter_drama</i>From Manifest Branch</div>
                           <div class="collapsible-body p-0">
+                            <div class="row">
+                              <div class="col s12 offset-m6 m6">
+                                <div class="app-wrapper ml-2 mt-2 mr-2">
+                                  <div class="datatable-search mb-0">
+                                    <i class="material-icons mr-2 search-icon">search</i>
+                                    <input type="text" placeholder="Search" class="app-filter" id="global_filter">
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
                             <div class="section-data-tables"> 
                               <table id="data-table-2" class="display" width="100%">
                                   <thead>
