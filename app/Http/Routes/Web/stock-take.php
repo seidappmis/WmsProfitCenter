ceeecf5c6312eeb4b0cdba81cd94ee4838f9ea0f
Rizki Fajri <muhamad.rizkifajri@gmail.com>
2020-04-23 10:14:57 +0700

    stok take input 1 done

--- app/Http/Routes/Web/stock-take.php
+++ app/Http/Routes/Web/stock-take.php
@@ -7,6 +7,8 @@ Route::group(['middleware' => 'auth'], function () {
   Route::view('stock-take-schedule/edit', 'web.stock-take.stock-take-schedule.edit');
 
   Route::view('stock-take-create-tag', 'web.stock-take.stock-take-create-tag.index');
+  Route::view('stock-take-create-tag/create', 'web.stock-take.stock-take-create-tag.create');
+
   Route::view('stock-take-input-1', 'web.stock-take.stock-take-input-1.index');
   Route::view('stock-take-input-2', 'web.stock-take.stock-take-input-2.index');
   Route::view('stock-take-quick-count', 'web.stock-take.stock-take-quick-count.index');
