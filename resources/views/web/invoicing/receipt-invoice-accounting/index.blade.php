d6e1a46233ae41bf437f65c3aac633576f518c3a
daniswarags <sukmagaluh@gmail.com>
2020-05-02 21:47:49 +0700

    rev

--- resources/views/web/invoicing/receipt-invoice-accounting/index.blade.php
+++ resources/views/web/invoicing/receipt-invoice-accounting/index.blade.php
@@ -7,10 +7,10 @@
     @component('layouts.materialize.components.title-wrapper')
         <div class="row">
             <div class="col s12 m6">
-                <h5 class="breadcrumbs-title mt-0 mb-0"><span>Recipt Invoice Accounting</span></h5>
+                <h5 class="breadcrumbs-title mt-0 mb-0"><span>Receipt Invoice Accounting</span></h5>
                 <ol class="breadcrumbs mb-0">
                     <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
-                    <li class="breadcrumb-item active">Recipt Invoice Accounting</li>
+                    <li class="breadcrumb-item active">Receipt Invoice Accounting</li>
                 </ol>
             </div>
             <div class="col s12 m6">
--- resources/views/web/invoicing/receipt-invoice/index.blade.php
+++ resources/views/web/invoicing/receipt-invoice/index.blade.php
@@ -7,7 +7,7 @@
     @component('layouts.materialize.components.title-wrapper')
         <div class="row">
             <div class="col s12 m6">
-                <h5 class="breadcrumbs-title mt-0 mb-0"><span>Recipt Invoice</span></h5>
+                <h5 class="breadcrumbs-title mt-0 mb-0"><span>Receipt Invoice</span></h5>
                 <ol class="breadcrumbs mb-0">
                     <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
                     <li class="breadcrumb-item active">Receipt Invoice List</li>
