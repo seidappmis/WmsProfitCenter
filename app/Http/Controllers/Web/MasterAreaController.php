efa6373b40e11f166cf6cb0188992fed27bd69ed
agungrizkis <agungsubhan@gmail.com>
2020-05-08 22:57:35 +0700

    fix conflict

--- app/Http/Controllers/Web/MasterAreaController.php
+++ app/Http/Controllers/Web/MasterAreaController.php
@@ -18,20 +18,6 @@ class MasterAreaController extends Controller
     public function index(Request $request)
     {
         if ($request->ajax()) {
-<<<<<<< HEAD
-            $query = MasterArea::all();
-
-            $datatables = DataTables::of($query)
-                ->addIndexColumn() //DT_RowIndex (Penomoran)
-                ->addColumn('action', function ($data) {
-                    $action = '';
-                    $action .= ' ' . get_button_edit(url('master-area/' . $data->id . '/edit'));
-                    $action .= ' ' . get_button_delete();
-                    return $action;
-                });
-
-            return $datatables->make(true);
-=======
           $query = MasterArea::all();
 
           $datatables = DataTables::of($query)
@@ -44,7 +30,6 @@ class MasterAreaController extends Controller
             });
 
           return $datatables->make(true);
->>>>>>> 1ad3443537980476d50b464e9b55bf7e7859ae3e
         }
         return view('web.settings.master-area.index');
     }
@@ -68,13 +53,8 @@ class MasterAreaController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-<<<<<<< HEAD
-            'code_area'  => 'max:10',
-            'area'       => 'max:100',
-=======
           'code'       => 'unique:master_areas|max:10',
           'area'       => 'max:100',
->>>>>>> 1ad3443537980476d50b464e9b55bf7e7859ae3e
         ]);
 
         $masterArea            = new MasterArea;
@@ -118,13 +98,8 @@ class MasterAreaController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-<<<<<<< HEAD
-            'code_area'  => 'max:10',
-            'area'       => 'max:100',
-=======
           'code'       => 'max:10',
           'area'       => 'max:100',
->>>>>>> 1ad3443537980476d50b464e9b55bf7e7859ae3e
         ]);
 
         $masterArea            = MasterArea::findOrFail($id);
@@ -153,13 +128,8 @@ class MasterAreaController extends Controller
     public function getSelect2Area(Request $request)
     {
         $query = MasterArea::select(
-<<<<<<< HEAD
-            'id',
-            DB::raw('area AS text')
-=======
           DB::raw('code AS id'),
           DB::raw('area AS text')
->>>>>>> 1ad3443537980476d50b464e9b55bf7e7859ae3e
         );
 
         return get_select2_data($request, $query);
