561074c4bb6a89b0a537da9ff2803f4fad8dd8ee
Rizki Fajri <muhamad.rizkifajri@gmail.com>
2020-05-09 16:37:22 +0700

    mastercabang form

--- resources/views/web/settings/master-cabang/_form.blade.php
+++ resources/views/web/settings/master-cabang/_form.blade.php
@@ -1,34 +1,4 @@
 <input type="hidden" name="id">
-<form class="form-table" id="form-master-cabang">
-    <table>
-        <tr>
-            <td>AREA</td>
-            <td>
-                <div class="input-field col s12">
-                    <input id="area"
-                    type="text"
-                    class="validate"
-                    name="area"
-                    value="{{old('area', !empty($masterArea) ? $masterArea->area : '')}}">
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td>CODE</td>
-            <td>
-                <div class="input-field col s12">
-                    <input id="code_area"
-                    type="text"
-                    class="validate"
-                    name="code_area"
-                    value="{{old('code_area', !empty($masterArea) ? $masterArea->code_area : '')}}">
-                </div>
-            </td>
-        </tr>
-    </table>
-    {!! get_button_save() !!}
-    {!! get_button_cancel(url('master-area')) !!}
-</form>
 
 <form class="form-table" id="form-master-cabang">
     <table>
