d80733d0db5481fe2bad0e08b171b8dd611a0c9d
daniswarags <sukmagaluh@gmail.com>
2020-05-21 06:55:16 +0700

    rev

--- app/Http/Controllers/Web/MasterModelController.php
+++ app/Http/Controllers/Web/MasterModelController.php
@@ -126,7 +126,40 @@ class MasterModelController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+          'model_name'        => 'required|max:50',
+          'model_from_apbar'  => 'max:50',
+          'ean_code'          => 'required|max:50',
+          'cbm'               => 'required',
+          'material_group'    => 'required',
+          'category'          => 'required',
+          'model_type'        => 'required',
+          'description'       => 'max:250',
+          'pcs_ctn'           => 'numeric',
+          'ctn_plt'           => 'numeric',
+          'max_pallet'        => 'numeric',
+          'price1'            => 'numeric',
+          'price2'            => 'numeric',
+          'price3'            => 'numeric',
+        ]);
+
+        $masterModel                   = MasterModel::findOrFail($id);
+        $masterModel->model_name       = $request->input('model_name' );
+        $masterModel->model_from_apbar = $request->input('model_from_apbar');
+        $masterModel->ean_code         = $request->input('ean_code');
+        $masterModel->cbm              = $request->input('cbm');
+        $masterModel->material_group   = $request->input('material_group');
+        $masterModel->category         = $request->input('category');
+        $masterModel->model_type       = $request->input('model_type');
+        $masterModel->description      = $request->input('description');
+        $masterModel->pcs_ctn          = $request->input('pcs_ctn');
+        $masterModel->ctn_plt          = $request->input('ctn_plt');
+        $masterModel->max_pallet       = $request->input('max_pallet');
+        $masterModel->price1           = $request->input('price1');
+        $masterModel->price2           = $request->input('price2');
+        $masterModel->price3           = $request->input('price3');
+
+        return $masterModel->save();
     }
 
     /**
