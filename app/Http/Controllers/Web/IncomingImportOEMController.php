0a86f039005d1438cfed59e9a3af4650418fa01e
okytagrit <okytagrit@gmail.com>
2020-08-12 18:01:25 +0700

    print incoming import oem

--- app/Http/Controllers/Web/IncomingImportOEMController.php
+++ app/Http/Controllers/Web/IncomingImportOEMController.php
@@ -302,18 +302,23 @@ class IncomingImportOEMController extends Controller
       $spreadsheet = $reader->loadFromString($view_print, $spreadsheet);
 
       // Set warna background putih
-      $spreadsheet->getActiveSheet()->getStyle('A1:G1000')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('ffffff');
+      $spreadsheet->getActiveSheet()->getStyle('A1:K1000')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('ffffff');
       // Set Font
-      $spreadsheet->getActiveSheet()->getStyle('A1:G1000')->getFont()->setName('courier New');
+      $spreadsheet->getActiveSheet()->getStyle('A1:K1000')->getFont()->setName('courier New');
 
       // Atur lebar kolom
       $spreadsheet->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
-      $spreadsheet->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
-      $spreadsheet->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+      $spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(5);
+      $spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(2);
       $spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(20);
-      $spreadsheet->getActiveSheet()->getColumnDimension('E')->setAutoSize(true);
-      $spreadsheet->getActiveSheet()->getColumnDimension('F')->setAutoSize(true);
-      $spreadsheet->getActiveSheet()->getColumnDimension('G')->setAutoSize(true);
+      // $spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(20);
+      $spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(5);
+      $spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(5);
+      $spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(5);
+      $spreadsheet->getActiveSheet()->getColumnDimension('H')->setWidth(5);
+      $spreadsheet->getActiveSheet()->getColumnDimension('I')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('J')->setWidth(2);
+      $spreadsheet->getActiveSheet()->getColumnDimension('K')->setAutoSize(true);
 
       $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
       header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
--- resources/views/web/incoming/incoming-import-oem/_print.blade.php
+++ resources/views/web/incoming/incoming-import-oem/_print.blade.php
@@ -1,35 +1,127 @@
 <link rel="stylesheet" type="text/css" href="{{ url('materialize/css/custom/print.css') }}">
 
-<table style="font-family: courier New; font-size: 10pt;">
+<table style="font-family: courier New; font-size: 8pt;">
   <tr>
     <td>
       <table style="width: 210.0003mm;">
         <tr>
           <td>
-            <table width="100%">
+            <table width="100%" style="font-family: courier New; font-size: 8pt;">
+              <tr><td>&nbsp;</td></tr>
+              <tr><td>&nbsp;</td></tr>
+              {{-- <tr><td style="height: 2px;"></td></tr> --}}
               <tr>
-                <td style="font-size: 12pt;" colspan="5"><strong>PT. SHARP ELECTRONICS INDONESIA <br> WAREHOUSE RECEIPT</strong></td>
-                <td style="font-size: 12pt;"><strong>OTHERS</strong></td>
+                <td style="font-size: 10pt;" rowspan="2" colspan="10"><strong>PT. SHARP ELECTRONICS INDONESIA <br> WAREHOUSE RECEIPT</strong></td>
+                <td style="font-size: 10pt; text-align: right;"><strong>IMPORT</strong></td>
               </tr>
               <tr><td></td></tr>
               <tr><td>&nbsp;</td></tr>
               <tr>
-                <td style="text-align: right;">Arrival No. :</td>
-                <td>OTHERS-WHJKT-200810-001</td>
-                <td style="text-align: right;">Vendor Name :</td>
-                <td>SHARP (PHILS) CORPORATION</td>
+                <td colspan="2" style="width: 30mm;">Arrival No.</td>
+                <td style="width: 5mm;">:</td>
+                <td colspan="4" style="width: 60mm;">IMPORT-WHJKT-191228-003</td>
+                <td colspan="2" style="width: 40mm;">Vendor Name</td>
+                <td style="width: 5mm;">:</td>
+                <td>SHARP TRADING INDONESIA, PT</td>
               </tr>
               <tr>
-                <td style="text-align: right;">PO :</td>
-                <td></td>
-                <td style="text-align: right;">Actual Arrive Date :</td>
-                <td>10-Aug-2020</td>
+                <td colspan="2">PO</td>
+                <td>:</td>
+                <td colspan="4">T0011912C</td>
+                <td colspan="2">Actual Arrive Date</td>
+                <td>:</td>
+                <td>28-Dec-2019</td>
               </tr>
               <tr>
-                <td style="text-align: right;">Invoice No. :</td>
+                <td colspan="2">Invoice No.</td>
+                <td>:</td>
+                <td colspan="4">AL69578</td>
+                <td colspan="2">Expedition Name</td>
+                <td>:</td>
                 <td></td>
-                <td style="text-align: right;">Expedition Name :</td>
-                <td></td>
+              </tr>
+              <tr>
+                <td colspan="2">No. GR SAP</td>
+                <td>:</td>
+                <td colspan="4" style="text-align: left;">5001331620</td>
+                <td colspan="2">Container No.</td>
+                <td>:</td>
+                <td>TCKU6075970</td>
+              </tr>
+              <tr>
+                <td colspan="2">Document Date</td>
+                <td>:</td>
+                <td colspan="4">28-Dec-2019</td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table width="100%" style="border-collapse: collapse; font-size: 8pt;">
+
+              {{-- Judul Tabel --}}
+              <tr>
+                <td style="text-align: center; border: 1pt solid #000000; width: 10mm;"><strong>No.</strong></td>
+                <td style="text-align: center; border: 1pt solid #000000; width: 50mm;" colspan="3"><strong>Model Name</strong></td>
+                <td style="text-align: center; border: 1pt solid #000000; width: 30mm;" colspan="2"><strong>CBM</strong></td>
+                <td style="text-align: center; border: 1pt solid #000000; width: 30mm;" colspan="2"><strong>Quantity</strong></td>
+                <td style="text-align: center; border: 1pt solid #000000; width: 30mm;"><strong>SLoc</strong></td>
+                <td style="text-align: center; border: 1pt solid #000000; width: 50mm;" colspan="2"><strong>Description</strong></td>
+              </tr>
+              {{-- AKHIR Judul Tabel --}}
+
+              <tr>
+                <td style="text-align: right; border: 1pt solid #000000;">1</td>
+                <td style="text-align: left; border: 1pt solid #000000;" colspan="3">4T-C50BK1X</td>
+                <td style="text-align: right; border: 1pt solid #000000;" colspan="2">0.161</td>
+                <td style="text-align: right; border: 1pt solid #000000;" colspan="2">100</td>
+                <td style="text-align: right; border: 1pt solid #000000;">1101</td>
+                <td style="text-align: left; border: 1pt solid #000000;" colspan="2">LED TV 50" ANDROID TV 4K HDR</td>
+                <td style="border-left: 1pt solid #000000;"></td>
+              </tr>
+              <tr>
+                <td style="text-align: center; border: 1pt solid #000000;" colspan="4">TOTAL</td>
+                <td style="text-align: right; border: 1pt solid #000000;" colspan="2">0.161</td>
+                <td style="text-align: right; border: 1pt solid #000000;" colspan="2">100</td>
+                <td style="text-align: right; border: 1pt solid #000000;"></td>
+                <td style="text-align: left; border: 1pt solid #000000;" colspan="2"></td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <table style="width: 210.0003mm;">
+        <tr>
+          <td>
+            <table style="font-family: courier New; font-size: 8pt;">
+              <tr>
+                <td colspan="3" style="width: 30mm;">TRANSFER BY</td>
+                <td style="width: 30mm; text-align: left; border-bottom: 1px solid #000000;">123</td>
+              </tr>
+              <tr>
+                <td colspan="3" style="width: 30mm;">CHECKED BY</td>
+                <td style="width: 30mm; text-align: left; border-bottom: 1px solid #000000;">123</td>
+              </tr>
+              <tr>
+                <td colspan="3" style="width: 30mm;">LOCATE</td>
+                <td style="width: 30mm; text-align: left; border-bottom: 1px solid #000000;">123</td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table style="font-family: courier New; font-size: 8pt;">
+              <tr>
+                <td colspan="11" style=" width: 200mm;text-align: right;">Wednesday, 12-August-2020 03:04:13 PM</td>
               </tr>
             </table>
           </td>
