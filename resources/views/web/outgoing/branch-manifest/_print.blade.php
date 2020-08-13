9c5ee12b85cb3192a6f66a0b8d0ed8502af9ea87
okytagrit <okytagrit@gmail.com>
2020-08-13 20:20:18 +0700

    print branch manifest

---
+++ resources/views/web/outgoing/branch-manifest/_print.blade.php
@@ -0,0 +1,176 @@
+<link rel="stylesheet" type="text/css" href="{{ url('materialize/css/custom/print.css') }}">
+
+<table style="font-family: Arial Narrow;">
+  <tr>
+    <td>
+      <table style="width: 210.0003mm;">
+        <tr>
+          <td>
+            <table width="100%" style="font-size: 9pt;">
+              <tr><td>&nbsp;</td></tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr>
+                <td colspan="17" style="font-size: 10pt;"><strong>PT. SHARP ELECTRONICS INDONESIA</strong></td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr>
+                <td colspan="17" style="font-size: 12pt; text-align: center;"><strong>BRANCH MANIFEST</strong></td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr>
+                <td>Manifest No.</td>
+                <td>:</td>
+                <td colspan="8" style="text-align: left; width: 90mm;"><strong>BDG-191228-012</strong></td>
+                <td colspan="2">Vehicle</td>
+                <td>:</td>
+                <td colspan="4" style="text-align: left;"><strong>D 8410 FE &nbsp;&nbsp;&nbsp; CD 6 BAN (CDD)</strong></td>
+              </tr>
+              <tr>
+                <td>Date</td>
+                <td>:</td>
+                <td colspan="8" style="text-align: left; width: 90mm;"><strong>28-December-2019</strong></td>
+                <td colspan="2">Expedition Name</td>
+                <td>:</td>
+                <td colspan="4" style="text-align: left;"><strong>PUTRA NAGITA PRATAMA</strong></td>
+              </tr>
+              <tr>
+                <td>Destination</td>
+                <td>:</td>
+                <td colspan="8" style="text-align: left; width: 90mm;"><strong>Tasikmalaya</strong></td>
+                <td colspan="2">Container No.</td>
+                <td>:</td>
+                <td colspan="4" style="text-align: left;"><strong>ini no container</strong></td>
+              </tr>
+              <tr>
+                <td>Picking No</td>
+                <td>:</td>
+                <td colspan="8" style="text-align: left; width: 90mm;"><strong>1320191223003</strong></td>
+                <td colspan="2">PDO. No</td>
+                <td>:</td>
+                <td colspan="4" style="text-align: left;"><strong>ini no PDO</strong></td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            {{-- Main Table --}}
+            <table  width="100%" style="font-size: 8pt; border-collapse: collapse;">
+              {{-- Table Head --}}
+              <tr>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;"><strong>Ship To</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;"><strong>Ship To - Detail</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; text-align: right;"><strong>#</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;"><strong>DO No.</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; text-align: right;"><strong>#</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;"><strong>Model</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; text-align: right;"><strong>Qty</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; text-align: right;"><strong>CBM</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; text-align: right;"><strong>Total CBM</strong></td>
+              </tr>
+              {{-- Table Body --}}
+              <tr>
+                <td rowspan="3" style="vertical-align: top;">13GC0200</td>
+                <td rowspan="3" style="width: 5mm;"></td>
+                <td rowspan="3" style="vertical-align: top;">CV GEMILANG JAYA &nbsp; BANDUNG</td>
+                <td rowspan="3" style="width: 5mm;"></td>
+                <td rowspan="3" style="vertical-align: top; text-align: right;">1.</td>
+                <td rowspan="3" style="width: 5mm;"></td>
+                <td rowspan="3" style="vertical-align: top;">2130060599</td>
+                <td rowspan="3" style="width: 5mm;"></td>
+                <td style="text-align: right;">1.</td>
+                <td style="width: 5mm;"></td>
+                <td>SJ-237ND-AB</td>
+                <td style="width: 5mm;"></td>
+                <td style="text-align: right;">5</td>
+                <td style="width: 5mm;"></td>
+                <td style="text-align: right;">0.555</td>
+                <td style="width: 5mm;"></td>
+                <td style="text-align: right;">2.775</td>
+              </tr>
+              <tr>
+                <td style="text-align: right;">2.</td>
+                <td style="width: 5mm;"></td>
+                <td>LC-40SA5100I</td>
+                <td style="width: 5mm;"></td>
+                <td style="text-align: right;">3</td>
+                <td style="width: 5mm;"></td>
+                <td style="text-align: right;">0.081</td>
+                <td style="width: 5mm;"></td>
+                <td style="text-align: right;">0.243</td>
+              </tr>
+              <tr>
+                <td style="text-align: right;">3.</td>
+                <td style="width: 5mm;"></td>
+                <td style="border-bottom: 1pt solid #000000;">SJ-N192D-AS</td>
+                <td style="border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-bottom: 1pt solid #000000; text-align: right;">2</td>
+                <td style="border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-bottom: 1pt solid #000000; text-align: right;">0.446</td>
+                <td style="border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-bottom: 1pt solid #000000; text-align: right;">0.892</td>
+              </tr>
+              <tr>
+                <td colspan="10"></td>`
+                <td style="text-align: right;"><strong>Sub Total</strong></td>
+                <td style="width: 5mm;"></td>
+                <td style="text-align: right;">10</td>
+                <td style="width: 5mm;"></td>
+                <td style="width: 5mm;"></td>
+                <td colspan="2" style="text-align: right;">3.910</td>
+              </tr>
+              <tr>
+                <td colspan="7" style="text-align: right; border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;"><strong>Total</strong></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; text-align: center;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; text-align: right;">1</td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td colspan="2" style="text-align: right; border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;">10</td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 5mm;"></td>
+                <td colspan="2" style="text-align: right; border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;">3.910</td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+            </table>
+            {{-- End Main Table --}}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table width="100%" style="font-size: 8pt;">
+              <tr>
+                <td colspan="3" style="text-align: center; width: 70mm;"><strong>EXPEDITION</strong></td>
+                <td colspan="8" style="text-align: center; width: 70mm;"><strong>OUTSOURCE <br> LOGISTIC</strong></td>
+                <td colspan="6" style="text-align: center; width: 70mm;"><strong>RECEIVER</strong></td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr>
+                <td colspan="3" style="text-align: center; width: 70mm;">(&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;)</td>
+                <td colspan="8" style="text-align: center; width: 70mm;">(&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;)</td>
+                <td colspan="6" style="text-align: center; width: 70mm;">(&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;)</td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr>
+                <td colspan="3" style="text-align: left; width: 70mm;">Thursday, 13-August-2020 04:16:31 PM</td>
+                <td colspan="8" style="text-align: center; width: 70mm;">Page 1 of 1</td>
+                <td colspan="6" style="text-align: right; width: 70mm;">print out from SEID WMS</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
