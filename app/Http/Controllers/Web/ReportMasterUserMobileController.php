5ccb356a3566d41b7e2467c9298288de787092c5
bobbysevri <bobbysevri@gmail.com>
2020-08-16 19:31:49 +0700

    rev

--- app/Http/Controllers/Web/ReportMasterUserMobileController.php
+++ app/Http/Controllers/Web/ReportMasterUserMobileController.php
@@ -32,7 +32,7 @@ class ReportMasterUserMobileController extends Controller
             $tabeldata .= '<tr><th>Username</th><th>name</th><th>Status</th><th>Privilage</th></tr>';
 
             foreach($query->get() AS $key => $value){
-                $tabeldata .= '<tr><td>' . $value->userid . '</td><td>'. $value->first_name . '</td><td>'. $value->status . '</td><td>'. $value->roles . '</td></tr>';
+                $tabeldata .= '<tr><td>' . $value->userid . '</td><td>'. $value->first_name . '</td><td>'. ($value->status ? 'Active' : 'Not Active') . '</td><td>'. ($value->roles ? 'Admin' : 'User') . '</td></tr>';
             }
 
             $tabeldata .= '</table>';
@@ -61,5 +61,69 @@ class ReportMasterUserMobileController extends Controller
           }
           return view('web.report.report-user-mobile.index');
     }
+    public function export(Request $request)
+  {
+    $query = UserScanner::select('wms_user_scanner.*','users.first_name','users.status')
+      ->leftjoin ('users','users.username','=', 'wms_user_scanner.userid')
+      ->leftjoin ('log_cabang','log_cabang.kode_customer','=', 'users.kode_customer')
+      ->where ('kode_cabang', $request->input('cabang'))
+      ->get();
+
+    $reader      = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+    $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+
+    foreach ($query as $key => $value) {
+      $reader->setSheetIndex($key);
+
+      // echo $value->username . '<br>';
+      $spreadsheet = $reader->loadFromString($this->getUserMobileTableData($value), $spreadsheet);
+
+      $spreadsheet->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+      $spreadsheet->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+      $spreadsheet->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+      $spreadsheet->getActiveSheet()->getColumnDimension('D')->setAutoSize(true);
+    }
+
+    $title = 'ReportUserMobile';
+
+    if ($request->input('file_type') == 'pdf') {
+      $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Mpdf');
+      header('Content-Type: application/pdf');
+      header('Content-Disposition: attachment;filename="' . $title . '.pdf"');
+      header('Cache-Control: max-age=0');
+    } else {
+      $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xls');
+      header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      header('Content-Disposition: attachment; filename="' . $title . '.xls"');
+    }
+
+    $writer->save("php://output");
+  }
+  protected function getUserMobileTableData($data)
+  {
+    
+    $table = '';
+    $table .= '<table  style="border-collapse: collapse; border: 1px solid black; width: 210mm;">';
+
+    $table .= '<tr style="border: 1px solid black;">';
+    $table .= '<th style="text-align: center; border: 1px solid black;">User Name</th>';
+    $table .= '<th style="text-align: center; border: 1px solid black;">Name</th>';
+    $table .= '<th style="text-align: center; border: 1px solid black;">Status</th>';
+    $table .= '<th style="text-align: center; border: 1px solid black;">Privilage</th>';
+    $table .= '</tr>';
+
+    $table .= '<tr style="border: 1px solid black;">';
+    $table .= '<td style="border: 1px solid black;">' . $data->userid . '</td>';
+    $table .= '<td style="border: 1px solid black;">' . $data->first_name . '</td>';
+    $table .= '<td style="border: 1px solid black;">' . ($data->status ? 'Active' : 'Not Active') . '</td>';
+    $table .= '<td style="border: 1px solid black;">' . ($data->roles ? 'Admin' : 'User') . '</td>';
+    $table .= '</tr>';
+
+    $table .= '</table>';
+
+
+    return $table;
+  }
 }
+
 
