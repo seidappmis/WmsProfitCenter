89d197260b35e96d329f26e7824b47b11885e4b9
daniswarags <sukmagaluh@gmail.com>
2020-08-18 14:23:44 +0700

    rev

---
+++ resources/views/web/claim/claim-insurance/_print_detail.blade.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" type="text/css" href="{{ url('materialize/css/custom/print.css') }}">
+
+<table style="font-family: Arial Narrow;">
+	<tr>
+		<td>
+			<table style="width: 210.0003mm;">
+				<tr>
+					<td>
+						<table width="100%" style="font-size: 9pt;">
+							<tr><td>&nbsp;</td></tr>
+							<tr>
+				                <td colspan="17" style="font-size: 12pt; text-align: center;"><strong>LOSS/DAMAGE PRODUCT LIST</strong></td>
+				            </tr>
+				            <tr>
+				                <td colspan="17" style="font-size: 12pt; text-align: center;"><strong>(Daftar Kerugian/Kerusakan Barang)</strong></td>
+				            </tr>
+				            <tr>
+				            	<td>&nbsp;</td>
+				            	<td><strong>1</strong></td>
+				            	<td>&nbsp;</td>
+				            	<td>&nbsp;</td>
+				            	<td>&nbsp;</td>
+				            	<td>&nbsp;</td>
+				            	<td>&nbsp;</td>
+				            	<td>DATE OF REPORT</td>
+				            	<td>03 Desember 2019</td>
+				            </tr>
+				            <tr>
+				            	<td>&nbsp;</td>
+				            	<td><strong>1st Report</strong></td>
+				            	<td>&nbsp;</td>
+				            	<td><strong>2nd Report</strong></td>
+				            	<td>&nbsp;</td>
+				            	<td>:</td>
+				            	<td>Wh Medan Des 2019</td>
+				            </tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
