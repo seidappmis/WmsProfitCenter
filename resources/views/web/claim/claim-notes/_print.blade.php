84533b7467cddc4570fb57bd409f10081817773c
daniswarags <sukmagaluh@gmail.com>
2020-08-19 14:21:00 +0700

    rev

--- resources/views/web/claim/claim-notes/_print.blade.php
+++ resources/views/web/claim/claim-notes/_print.blade.php
@@ -1,16 +1,15 @@
 <link rel="stylesheet" type="text/css" href="{{ url('materialize/css/custom/print.css') }}">
 
-<table style="font-family: Arial Narrow; font-size: 10pt; border-collapse: collapse; border: 2px solid #000000;">
+<table style="font-family: Arial Narrow; font-size: 10pt;  border-collapse: collapse;">
 	<tr>
 		<td>
-			<table style="width: 210.0003mm;">
+			<table style="width: 210.0003mm; border-collapse: collapse; border: 2px solid #000000;">
 				<tr>
 					<td>
 						<table width="100%">
               				<tr>
 				                <td><img src="{{ url('images/sharp-logo.png') }}" alt="sharp-logo"  style="width: 100px;"></td>
 				            </tr>
-              				<!-- <tr><td style="height: 2px;"></td></tr> -->
               				<tr>
 				                <td style="text-align: center; font-size: 18pt;" colspan="15"><strong>CLAIM LETTER</strong></td>
 				            </tr>
@@ -26,7 +25,6 @@
 			              	</tr>
 			              	<tr>
 			              		<td style="text-align: left; width: 20mm;"><strong>Issued by :</strong></td>
-			              		<!-- <td style="width: 3mm;"><strong>:</strong></td> -->
 			              		<td style="text-align: center; width: 40mm; border-bottom: 1px solid #000000;"><strong>LOGISTICS</strong></td>
 			              		<td style="width: 3mm;">&nbsp;</td>
 			              		<td style="text-align: left; width: 10mm;"><strong>Division</strong></td>
@@ -44,34 +42,118 @@
 					<td>
 						{{-- Table --}}
 						<table width="100%" style="border-collapse: collapse;">
+							{{-- Head --}}
 							<tr>
-								<td style="border-top: 2pt solid #000000; border-bottom: 2pt solid #000000;">
-									<table width="100%" style="border-collapse: collapse;">
-										<tr>
-											<td><strong>Plant No :</strong></td>
-											<td>&nbsp;</td>
-											<td>&nbsp;</td>
-										</tr>
-									</table>
-								</td>
-								<td style="border-top: 2pt solid #000000; border-bottom: 2pt solid #000000; border-left: 1pt solid #000000;">
-									<table width="100%" style="border-collapse: collapse;">
-										<tr>
-											<td><strong>Part Code :</strong></td>
-											<td style="border-left: 1pt solid #000000;"><strong>Part Name:</strong></td>
-											<td style="border-left: 1pt solid #000000;"><strong>Mould Name :</strong></td>
-										</tr>
-									</table>
-								</td>
+								<td style="border-top: 2pt solid #000000; border-bottom: 2pt solid #000000;  width: 80mm;"><strong>Plan No :</strong></td>
+                				<td style="border-top: 2pt solid #000000; border-bottom: 2pt solid #000000; border-left: 1pt solid #000000;"><strong>Part Code :</strong></td>
 							</tr>
-							<tr><td>&nbsp;</td></tr>
+							{{-- Body --}}
 							<tr>
-								<td>
-									Reason:
-								</td>
+								<td style="border-top: 2pt solid #000000;">Reason :</td>
+                				<td style="border-top: 2pt solid #000000; border-left: 1pt solid #000000;">Claim Amount :</td>
+							</tr>
+							<tr>
+								<td><strong>Claim Carton Box :</strong></td>
+                				<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td>Wet Carton Box</td>
+                				<td style="border-left: 1pt solid #000000;">Material Cost Amount</td>
+							</tr>
+							<tr>
+								<td>Damage Carton Box</td>
+                				<td style="border-left: 1pt solid #000000;">F/G Sales Price Amount</td>
+							</tr>
+							<tr>
+								<td><strong>Claim Unit : &nbsp; 2 Unit</strong></td>
+                				<td style="border-left: 1pt solid #000000;">Man Power Cost</td>
+							</tr>
+							<tr>
+								<td>Unit of F/G Damaged</td>
+                				<td style="border-left: 1pt solid #000000;">Other Cost</td>
+							</tr>
+							<tr>
+								<td>Unit of F/G Scratched</td>
+                				<td style="border-left: 1pt solid #000000;">Claim Cost (1+2+3+4)</td>
+							</tr>
+							<tr>
+								<td>Unit of F/G Dented</td>
+                				<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td>Unit of F/G Broken</td>
+                				<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td>Other Problem</td>
+                				<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td><strong>Remarks :</strong></td>
+								<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td style="border-left: 1pt solid #000000;"></td>
+							</tr>
+							<tr>
+								<td style="border-bottom: 2pt solid #000000;">&nbsp;</td>
+                				<td style="border-left: 1pt solid #000000; border-bottom: 2pt solid #000000; font-size: 8pt;">Note: Prices are subject to change without priot notice</td>
+							</tr>
+							<tr>
+								<td style="border-bottom: 2pt solid #000000;"><strong>Total Claim Amount</strong></td>
+								<td style="border-bottom: 2pt solid #000000;">&nbsp;</td>
+							</tr>
+							<tr>
+								<td style="border-bottom: 2pt solid #000000;"><strong>Logistic Dept. Opinion :</strong></td>
+								<td style="border-bottom: 2pt solid #000000;">&nbsp;</td>
+							</tr>
+							<tr><td style="height: 1px;"></td></tr>
+							<tr>
+								<td style="border-top: 2pt solid #000000; border-bottom: 2pt solid #000000;"><strong>Company Name :</strong></td>
+								<td style="border-top: 2pt solid #000000; border-bottom: 2pt solid #000000;">&nbsp;</td>
+							</tr>
+							<tr><td style="height: 1px;"></td></tr>
+							<tr>
+								<td style="border-top: 2pt solid #000000;"><strong>Accounting Dept. Opinion :</strong></td>
+								<td style="border-top: 2pt solid #000000;">&nbsp;</td>
+							</tr>
+							<tr>
+								<td style="border-top: 2pt solid #000000;"><strong>Management Opinion :</strong></td>
+								<td style="border-top: 2pt solid #000000;">&nbsp;</td>
+							</tr>
+							<tr>
+								<td style="border-top: 2pt solid #000000;"><strong>Debit Note No :</strong></td>
+								<td style="border-top: 2pt solid #000000;">&nbsp;</td>
 							</tr>
 						</table>
 					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<table style="width: 210.0003mm; border-collapse: collapse;">
+				<tr>
+					<td style="font-size: 7pt;">Note: Harga diatas dapat berubah sewaktu-waktu tanpa pemberitahuan sebelumnya</td>
+					<td style="text-align: center; font-size: 9pt; border-bottom: 2pt solid #000000; border-left: 2pt solid #000000; border-right: 2pt solid #000000; width: 30mm;">QUALITY-FORM-003</td>
 				</tr>
 			</table>
 		</td>
