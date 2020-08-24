b00d4611e5cb86cb7d1deb4eb4d9f69569b5f7a4
daniswarags <sukmagaluh@gmail.com>
2020-08-24 15:03:34 +0700

    rev

--- resources/views/web/claim/claim-insurance/_print_rpt.blade.php
+++ resources/views/web/claim/claim-insurance/_print_rpt.blade.php
@@ -9,12 +9,14 @@
 						<table width="100%" style="font-size: 9pt;">
 							<tr><td>&nbsp;</td></tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td>TO</td>
 								<td>:</td>
 								<td>PT. ASURANSI MSIG INDONESIA</td>
 								<td>CLAIM REPORT NO : C27031219</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td>FAX</td>
 								<td>:</td>
 								<td>(021) 252-4084, 252-4083 (MARINE CLAIM SECTION)</td>
@@ -31,10 +33,13 @@
 					<td>
 						<table width="100%" style="font-size: 12pt; border-collapse: collapse;">
 							<tr>
+								<td>&nbsp;</td>
 								<td colspan="12" style="text-align: center; border: 1px solid #000000"><strong>DATE OF REPORT : 03 Desember 2019</strong></td>
+								<td>&nbsp;</td>
 							</tr>
 							<tr><td>&nbsp;</td></tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td colspan="7" style="text-align: center; border: 2px solid #000000"><strong>1st REPORT (within 2 days)</strong></td>
 								<td style="width: 20mm;">&nbsp;</td>
 								<td style="width: 20mm;">&nbsp;</td>
@@ -52,7 +57,7 @@
 								<td>&nbsp;</td>
 								<td>&nbsp;</td>
 								<td>&nbsp;</td>
-								<td rowspan="2" colspan="8" style="text-align: left; color: red;">" Please fill in this 1st Report within 2 days after the accident happened and submit to HO and Insurance Company by e-mail"</td>
+								<td rowspan="2" colspan="8" style="text-align: left; color: red;">" Please fill in this 1st Report <u>within 2 days after the accident happened</u> and submit to HO and Insurance Company by e-mail"</td>
 							</tr>
 						</table>
 					</td>
@@ -62,13 +67,14 @@
 						{{-- Table --}}
 						<table width="100%" style="font-size: 9pt; border-collapse: collapse;">
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;"><strong>Branch</strong></td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 10mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;"><strong>: Wh Medan Des 2019</strong></td>
-							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;"><strong>Date of Loss</strong></td>
 								<td style="border-top: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -76,6 +82,7 @@
 								<td style="border-top: 1pt solid #000000; border-right: 1pt solid #000000;"></td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;">(when the accident happened/found)</td>
 								<td style="border-bottom: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -83,6 +90,7 @@
 								<td style="border-bottom: 1pt solid #000000; border-right: 1pt solid #000000; color: blue;"><strong>: Des 2019 ( Input )</strong></td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;"><strong>Damage Product</strong></td>
 								<td style="border-top: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -90,6 +98,7 @@
 								<td style="border-top: 1pt solid #000000; border-right: 1pt solid #000000; color: blue;">: Please fill up the attachment</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;">(product & serial number)</td>
 								<td style="border-bottom: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -97,6 +106,7 @@
 								<td style="border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;"><strong>Place of Loss Found</strong></td>
 								<td style="border-top: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -104,6 +114,7 @@
 								<td style="border-top: 1pt solid #000000; border-right: 1pt solid #000000; color: blue;">: Please fill up the attachment</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;">(office/warehouse/dealer)/shop/transit)</td>
 								<td style="border-bottom: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -111,6 +122,7 @@
 								<td style="border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;"><strong>Route</strong></td>
 								<td style="border-top: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -118,6 +130,7 @@
 								<td style="border-top: 1pt solid #000000; border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-left: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="width: 40mm;"><strong>From</strong></td>
 								<td style="width: 10mm;">&nbsp;</td>
@@ -125,6 +138,7 @@
 								<td style="border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-left: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="width: 40mm;">(at the first time the product leave)</td>
 								<td style="width: 10mm;">&nbsp;</td>
@@ -132,6 +146,7 @@
 								<td style="border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-left: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="width: 40mm;"><strong>To</strong></td>
 								<td style="width: 10mm;">&nbsp;</td>
@@ -139,6 +154,7 @@
 								<td style="border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; width: 40mm;">(final destination)</td>
 								<td style="border-bottom: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -146,6 +162,7 @@
 								<td style="border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;"><strong>Nature of Loss</strong></td>
 								<td style="border-top: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -153,18 +170,21 @@
 								<td style="border-top: 1pt solid #000000; border-right: 1pt solid #000000; color: blue;">: Broken/bend/dent/scratch/stolen/w et/carton box etc/ other / accident <strong>(Pilih)</strong></td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td colspan="2" style="border-bottom: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;">(choose the nature of loss or mention if the circumstance is other)</td>
 								<td style="border-bottom: 1pt solid #000000; width: 10mm;">&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000;">&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td colspan="2" style="border-top: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;"><strong>Other Info (if any, such as : name of transportation coy & driver)</strong></td>
 								<td style="border-top: 1pt solid #000000; width: 10mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-left: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="width: 40mm;">&nbsp;</td>
 								<td style="width: 10mm;">&nbsp;</td>
@@ -172,10 +192,12 @@
 								<td style="border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-left: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td colspan="4" style="border-right: 1pt solid #000000; width: 40mm;"><strong>Unit Lost (Bajing Loncat) ( Input di "Keterangan kejadian" )</strong></td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-bottom: 1pt solid #000000; width: 10mm;">&nbsp;</td>
@@ -183,20 +205,152 @@
 								<td style="border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">&nbsp;</td>
 							</tr>
 							<tr>
+								<td>&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; border-left: 1pt solid #000000; width: 40mm;"><strong>Photograph</strong></td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 40mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; width: 10mm;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;">&nbsp;</td>
 								<td style="border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000; color: blue;">please attach some picture to show the overall situation of the damage/loss</td>
+								<td>&nbsp;</td>
 							</tr>
 						</table>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<table>
+						<table width="100%" style="font-size: 8pt;">
+							<tr><td>&nbsp;</td></tr>
 							<tr>
-								<td>tes</td>
+								<td>&nbsp;</td>
+								<td colspan="2" style="text-align: left; width: 50mm;"><strong>Reported by</strong></td>
+				                <td colspan="2" style="text-align: center; width: 70mm;"><strong>Acknowledged,</strong></td>
+				                <td colspan="3" style="text-align: center; width: 70mm;"><strong>Acknowledged,</strong></td>
+				                <td colspan="3" style="text-align: center; width: 70mm;"><strong>Acknowledged</strong></td>
+				                <td colspan="3" style="text-align: right; width: 70mm;"><strong>Acknowledged</strong></td>
+				                <td>&nbsp;</td>
+							</tr>
+							<tr><td>&nbsp;</td></tr>
+				            <tr><td>&nbsp;</td></tr>
+				            <tr><td>&nbsp;</td></tr>
+				            <tr>
+								<td>&nbsp;</td>
+								<td colspan="2" style="text-align: left; width: 50mm;"><strong>(Hardian)</strong></td>
+								<td style="text-align: left; width: 20mm;"><strong>(Tomi S) <br> Manager</strong></td>
+				                <td style="text-align: left; width: 35mm;"><strong>(Firman) <br> Manager</strong></td>
+				                <td colspan="3" style="text-align: center; width: 70mm;"><strong>(Denny A.R ) <br> Logistic GM</strong></td>
+				                <td colspan="3" style="text-align: center; width: 70mm;"><strong>( K. Tani ) <br> Acc GM</strong></td>
+				                <td colspan="3" style="text-align: right; width: 70mm;"><strong>(Sayloom Labiro) <br> Sr.Mgr.Acc Tax & Fin</strong></td>
+				                <td>&nbsp;</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table width="100%" style="font-size: 12pt; border-collapse: collapse;">
+							<tr><td>&nbsp;</td></tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td colspan="7" style="text-align: center; border: 2px solid #000000"><strong>2nd REPORT (within a week)</strong></td>
+								<td style="width: 20mm;">&nbsp;</td>
+								<td style="width: 20mm;">&nbsp;</td>
+								<td style="width: 20mm;">&nbsp;</td>
+								<td style="width: 20mm;">&nbsp;</td>
+								<td style="width: 20mm;">&nbsp;</td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td colspan="2" style="text-align: left; color: red;"><strong>IMPORTANT :</strong></td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td rowspan="2" colspan="8" style="text-align: left; color: red;">" These below supporting documents should be prepared and submitted to Insurance Company <u>within <strong>a week after the accident happened"</strong></u></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table width="100%">
+							<tr><td>&nbsp;</td></tr>
+							<tr>
+								<td colspan="6"><strong>SUPPORTING DOCUMENTS :</strong></td>
+							</tr>
+							<tr><td>&nbsp;</td></tr>
+							<tr>
+								<td colspan="2" style="border: 1pt solid #000000; width: 20mm; font-size: 11pt;"></td>
+								<td style="width: 10mm;">&nbsp;</td>
+								<td style="font-size: 9pt;"><strong>Delivery Order</strong> (Surat Jalan)</td>
+								<td style="width: 30mm;">&nbsp;</td>
+								<td style="width: 5mm; font-size: 9pt;"><strong>:</strong></td>
+								<td style="width: 80mm;">&nbsp;</td>
+							</tr>
+							<tr><td>&nbsp;</td></tr>
+							<tr>
+								<td colspan="2" style="border: 1pt solid #000000; width: 20mm; font-size: 11pt;"></td>
+								<td style="width: 10mm;">&nbsp;</td>
+								<td style="font-size: 9pt; width: 60mm;"><strong>Return Form</strong> (Tanda Terima Barang Cacat)</td>
+								<td style="width: 30mm;">&nbsp;</td>
+								<td style="width: 5mm; font-size: 9pt;"><strong>:</strong></td>
+								<td style="width: 80mm;">&nbsp;</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table  width="100%" style="font-size: 9pt;">
+							<tr><td>&nbsp;</td></tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td colspan="5" style="text-align: left; width: 50mm;"><strong><i>In Head Office, please fill in the Declaration and Comment if any,</i></strong></td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+				                <td colspan="2" style="text-align: left; width: 50mm;"><strong>Acknowledged,</strong></td>
+				                <td colspan="2" style="text-align: center; width: 40mm;">&nbsp;</td>
+				                <td colspan="5" style="text-align: center; width: 100mm;"><strong>Declaration for ( month/year) : ON Des 2019</strong></td>
+				                <td colspan="3" style="text-align: center; width: 70mm;">&nbsp;</td>
+				                <td colspan="3" style="text-align: right; width: 70mm;">&nbsp;</td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+				                <td colspan="3" style="text-align: left; width: 50mm;">&nbsp;</td>
+				                <td colspan="5" style="text-align: center; width: 100mm;"><strong>Comment :</strong></td>
+				                <td colspan="3" style="text-align: center; width: 70mm;">&nbsp;</td>
+				                <td colspan="3" style="text-align: right; width: 70mm;">&nbsp;</td>
+							</tr>
+							<tr><td>&nbsp;</td></tr>
+            				<tr><td>&nbsp;</td></tr>
+            				<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+				                <td colspan="2" style="text-align: left; width: 50mm;"><strong>(................................)</strong></td>
+				                <td colspan="2" style="text-align: center; width: 40mm;">&nbsp;</td>
+				                <td colspan="5" style="text-align: center; width: 100mm;">&nbsp;</td>
+				                <td colspan="3" style="text-align: center; width: 70mm;">&nbsp;</td>
+				                <td colspan="3" style="text-align: right; width: 70mm;">&nbsp;</td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+				                <td colspan="2" style="text-align: left; width: 50mm;"><strong>Authorized Person</strong></td>
+				                <td colspan="2" style="text-align: center; width: 40mm;">&nbsp;</td>
+				                <td colspan="5" style="text-align: center; width: 100mm;">&nbsp;</td>
+				                <td colspan="3" style="text-align: center; width: 70mm;">&nbsp;</td>
+				                <td colspan="3" style="text-align: right; width: 70mm;">&nbsp;</td>
 							</tr>
 						</table>
 					</td>
