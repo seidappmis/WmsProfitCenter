2b8e33db892b2384755b586ebe4a1894d0cd1237
agungrizkis <agungsubhan@gmail.com>
2020-08-26 01:19:28 +0700

    rev

--- resources/views/web/dashboard/dashboard1/_loading_daily_status_graph.blade.php
+++ resources/views/web/dashboard/dashboard1/_loading_daily_status_graph.blade.php
@@ -1,14 +1,20 @@
 <div id="Chart1_container"></div>
 
-@push('script_js')
-<script src="{{asset('vendors/highcharts/highcharts.js')}}"></script>
-<script src="{{asset('vendors/highcharts/drilldown.src.js')}}"></script>
-<script src="{{asset('vendors/highcharts/grouped-categories.js')}}"></script>
-<script src="{{asset('vendors/highcharts/jquery.highchartTable.js')}}"></script>
-<script src="{{asset('vendors/highcharts/modules/exporting.js')}}"></script>
-<script src="{{asset('vendors/highcharts/modules/export-data.js')}}"></script>
-<script src="{{asset('vendors/highcharts/modules/accessibility.js')}}"></script>
+@push('script_css')
+<link rel="stylesheet" type="text/css" href="{{ url('vendors/highchart/highcharts.css') }}">
+@endpush
 
+@push('vendor_js')
+<script src="{{asset('vendors/highchart/highcharts.js')}}"></script>
+<script src="{{asset('vendors/highchart/drilldown.src.js')}}"></script>
+<script src="{{asset('vendors/highchart/grouped-categories.js')}}"></script>
+<script src="{{asset('vendors/highchart/modules/exporting.js')}}"></script>
+{{-- <script src="{{asset('vendors/highchart/modules/export-data.js')}}"></script> --}}
+<script src="{{asset('vendors/highchart/jquery.highchartTable.js')}}"></script>
+{{-- <script src="{{asset('vendors/highchart/modules/accessibility.js')}}"></script> --}}
+@endpush
+
+@push('script_js')
 <script type="text/javascript">
   var cbm_of_concept = [{ y: 0 }, { y: 780 }, { y: 97 }, { y: 346 }, { y: 6 }, { y: 3149 }, { y: 127 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 315 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 206 }, { y: 0 }, { y: 0 }, { y: 64 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 70 }, { y: 0 }, { y: 0 }, { y: 65 }, { y: 86 }, { y: 16 }, { y: 0 }, { y: 450 }, { y: 0 }];
   var cbm_of_truck = [{ y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 325 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 205 }, { y: 0 }, { y: 0 }, { y: 65 }, { y: 0 }, { y: 0 }, { y: 0 }, { y: 65 }, { y: 0 }, { y: 0 }, { y: 65 }, { y: 108 }, { y: 25 }, { y: 0 }, { y: 490 }, { y: 0 }]
@@ -22,7 +28,17 @@
     xAxis: {
       categories: [
         { name: 'Waiting Truck', categories: ['8 METER', 'CONTAINER', 'LIGHT TRUCK', 'SMALL TRUCK', 'TRAILER', 'TRONTON', 'WINGBOX'] }, { name: 'Waiting Concept', categories: ['8 METER', 'CONTAINER', 'LIGHT TRUCK', 'SMALL TRUCK', 'TRAILER', 'TRONTON', 'WINGBOX'] }, { name: 'Waiting Loading', categories: ['8 METER', 'CONTAINER', 'LIGHT TRUCK', 'SMALL TRUCK', 'TRAILER', 'TRONTON', 'WINGBOX'] }, { name: 'Loading Process', categories: ['8 METER', 'CONTAINER', 'LIGHT TRUCK', 'SMALL TRUCK', 'TRAILER', 'TRONTON', 'WINGBOX'] }, { name: 'Waiting D/O', categories: ['8 METER', 'CONTAINER', 'LIGHT TRUCK', 'SMALL TRUCK', 'TRAILER', 'TRONTON', 'WINGBOX'] },
-      ]
+      ],
+      labels: {
+        groupedOptions: [{
+            style: {
+                color: 'black', // set red font for labels in 1st-Level  
+            },
+            rotation: 0, // rotate labels for a 2nd-level
+            align: 'center'
+        }],
+        rotation: 90 // 0-level options aren't changed, use them as always
+    },
     },
     yAxis: { min: 0, title: { text: '' } },
     series: [{ data: cbm_of_truck, name: 'CBM of Truck' }, { data: cbm_of_concept, name: 'CBM of Concept' }]
