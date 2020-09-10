0940599c0327e73e39e7dfcf729481d5b70cf057
okytagrit <okytagrit@gmail.com>
2020-09-11 04:05:02 +0700

    print ba during attach

--- app/Http/Controllers/Web/BeritaAcaraDuringController.php
+++ app/Http/Controllers/Web/BeritaAcaraDuringController.php
@@ -16,7 +16,63 @@ class BeritaAcaraDuringController extends Controller
   {
     // $data['pickinglistHeader'] = PickinglistHeader::findOrFail($id);
 
-    $view_print = view('web.during.berita-acara-during._print');
+    $view_print = view('web.during.berita-acara-during._print_BA');
+    $title      = 'Berita Acara Barang During';
+
+    if ($request->input('filetype') == 'html') {
+
+      // request HTML View
+      return $view_print;
+
+    } elseif ($request->input('filetype') == 'xls') {
+
+      // Request FILE EXCEL
+      $reader      = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+      $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+
+      $spreadsheet = $reader->loadFromString($view_print, $spreadsheet);
+
+      // Set warna background putih
+      $spreadsheet->getDefaultStyle()->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('ffffff');
+      // Set Font
+      $spreadsheet->getDefaultStyle()->getFont()->setName('courier New');
+
+      // Atur lebar kolom
+      $spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(15);
+      $spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(5);
+      $spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(25);
+      $spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(15);
+      $spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(30);
+      $spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(30);
+      $spreadsheet->getActiveSheet()->getColumnDimension('H')->setWidth(30);
+
+      $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
+      header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      header('Content-Disposition: attachment; filename="' . $title . '.xls"');
+
+      $writer->save("php://output");
+
+    } else if ($request->input('filetype') == 'pdf') {
+
+      // REQUEST PDF
+      $mpdf = new \Mpdf\Mpdf(['tempDir' => '/tmp']);
+
+      $mpdf->WriteHTML($view_print, \Mpdf\HTMLParserMode::HTML_BODY);
+
+      $mpdf->Output($title . '.pdf', "D");
+
+    } else {
+      // Parameter filetype tidak valid / tidak ditemukan return 404
+      return redirect(404);
+    }
+  }
+
+  public function exportAttach(Request $request, $id)
+  {
+    // $data['pickinglistHeader'] = PickinglistHeader::findOrFail($id);
+
+    $view_print = view('web.during.berita-acara-during._print_attach');
     $title      = 'Berita Acara Barang During';
 
     if ($request->input('filetype') == 'html') {
---
+++ resources/views/web/during/berita-acara-during/_print_attach.blade.php
@@ -0,0 +1,98 @@
+<link rel="stylesheet" type="text/css" href="{{ url('materialize/css/custom/print.css') }}">
+
+<table style="font-family: Arial;">
+  <tr>
+    <td>
+      <table style="width: 210.0003mm;">
+        <tr>
+          <td>
+            <table width="100%" style=" font-size: 10pt;">
+              <tr><td>&nbsp;</td></tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr><td style="font-size: 12pt; text-align: center;"><strong>BERITA ACARA BARANG DURING</strong></td></tr>
+              <tr><td style="font-size: 12pt; text-align: center;"><strong>No : 037/DR-SWD/XII/2019</strong></td></tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr>
+                <td></td>
+                <td>No. Container</td>
+                <td>:</td>
+                <td>DRYU9848459</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Jenis Kerusakan</td>
+                <td>:</td>
+                <td>Karton Rusak</td>
+              </tr>
+              <tr>
+                <td>Logistic &amp; Distribution Section</td>
+                <td><strong>B. Rusak dari dalam container(Import)</strong></td>
+              </tr>
+              <tr>
+                <td>PT.Sharp Trading Indonesia</td>
+                <td><strong>Tanggal Kejadian</strong></td>
+                <td><strong>:</strong></td>
+                <td><strong>09 Desember 2019</strong></td>
+              </tr>
+              <tr>
+                <td>Jakarta</td>
+                <td><strong>Tanggal Report</strong></td>
+                <td><strong>:</strong></td>
+                <td><strong>09 Desember 2019</strong></td>
+              </tr>
+              <tr><td>&nbsp;</td></tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table width="100%" style="font-size: 10pt; border-collapse: collapse;">
+              <tr>
+                <td style="text-align: center; border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; border-left: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-top: 1pt solid #000000; border-bottom: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-top: 1pt solid #000000; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">img</td>
+              </tr>
+              <tr>
+                <td style="text-align: center; border-left: 1pt solid #000000;">R-21DO(S)IN</td>
+                <td style="text-align: center; border-right: 1pt solid #000000;">911411357</td>
+                <td style="text-align: center;">R-21DO(S)IN</td>
+                <td style="text-align: center; border-right: 1pt solid #000000;">911411357</td>
+              </tr>
+              <tr>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-left: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">img</td>
+              </tr>
+              <tr>
+                <td style="text-align: center; border-left: 1pt solid #000000;">R-21DO(S)IN</td>
+                <td style="text-align: center; border-right: 1pt solid #000000;">911411357</td>
+                <td style="text-align: center;">R-21DO(S)IN</td>
+                <td style="text-align: center; border-right: 1pt solid #000000;">911411357</td>
+              </tr>
+              <tr>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-left: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">img</td>
+              </tr>
+              <tr>
+                <td style="text-align: center; border-left: 1pt solid #000000;">R-21DO(S)IN</td>
+                <td style="text-align: center; border-right: 1pt solid #000000;">911411357</td>
+                <td style="text-align: center;">R-21DO(S)IN</td>
+                <td style="text-align: center; border-right: 1pt solid #000000;">911411357</td>
+              </tr>
+              <tr>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-left: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000;">img</td>
+                <td style="text-align: center; border-bottom: 1pt solid #000000; border-right: 1pt solid #000000;">img</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
