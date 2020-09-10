79dc8f051020a6a08ca3fd1a727380f6b60c9ade
okytagrit <okytagrit@gmail.com>
2020-09-11 06:45:45 +0700

    print dgr

--- app/Http/Routes/Web/during.php
+++ app/Http/Routes/Web/during.php
@@ -7,5 +7,6 @@ Route::group(['middleware' => 'auth'], function () {
   Route::get('/berita-acara-during/{id}/export-attach', 'Web\BeritaAcaraDuringController@exportAttach');
 
   Route::get('/damage-goods-report', 'Web\DamageGoodsReportController@index');
+  Route::get('/damage-goods-report/{id}/export', 'Web\DamageGoodsReportController@export');
  
 });
