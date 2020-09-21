b5f53235af4586a768d21121b9c1eb56b636960c
okytagrit <okytagrit@gmail.com>
2020-09-21 22:03:12 +0700

    print report occupancy

--- app/Http/Controllers/Web/ReportOccupancyController.php
+++ app/Http/Controllers/Web/ReportOccupancyController.php
@@ -17,6 +17,75 @@ class ReportOccupancyController extends Controller
 
   public function export(Request $request)
   {
-    return view('web.report.report-occupancy.print');
+    // return view('web.report.report-occupancy.print');
+
+    // $data['request'] = $request->all();
+    // $data['header']  = LogReturnSuratTugasHeader::findOrFail($id);
+
+    // $view_print = view('web.report.summary-wh-transporter-report.print', $data);
+    $view_print = view('web.report.report-occupancy.print');
+    $title      = 'Report Occupancy';
+
+    if ($request->input('filetype') == 'html') {
+
+      // request HTML View
+      return $view_print;
+
+    } elseif ($request->input('filetype') == 'xls') {
+
+      // Request FILE EXCEL
+      $reader      = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+      $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+
+      $spreadsheet = $reader->loadFromString($view_print, $spreadsheet);
+
+      // Set warna background putih
+      $spreadsheet->getDefaultStyle()->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('ffffff');
+      // Set Font
+      $spreadsheet->getDefaultStyle()->getFont()->setName('courier New');
+
+      // Atur lebar kolom
+      $spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(30);
+      $spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(15);
+      $spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(15);
+      $spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('H')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('I')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('J')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('K')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('L')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('M')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('N')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('O')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('P')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('Q')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('R')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('S')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('T')->setWidth(10);
+      $spreadsheet->getActiveSheet()->getColumnDimension('U')->setWidth(10);
+    
+      $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
+      header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      header('Content-Disposition: attachment; filename="' . $title . '.xls"');
+
+      $writer->save("php://output");
+
+    } else if ($request->input('filetype') == 'pdf') {
+
+      // REQUEST PDF
+      $mpdf = new \Mpdf\Mpdf(['tempDir' => '/tmp']);
+
+      $mpdf->WriteHTML($view_print, \Mpdf\HTMLParserMode::HTML_BODY);
+
+      $mpdf->Output($title . '.pdf', "D");
+
+    } else {
+      // Parameter filetype tidak valid / tidak ditemukan return 404
+      return redirect(404);
+    }
+  // }
   }
 }
---
+++ resources/views/web/report/report-occupancy/print.blade.php
@@ -0,0 +1,98 @@
+<link rel="stylesheet" type="text/css" href="{{ url('materialize/css/custom/print.css') }}">
+
+<table style="font-family: Arial;">
+  <tr>
+    <td>
+      <table>
+        <tr>
+          <td>
+            <table width="100%" style="font-size: 9pt; border-collapse: collapse;">
+              <tr><td>&nbsp;</td></tr>
+              <tr><td>&nbsp;</td></tr>
+              <tr>
+                <td rowspan="3" style="border: 1pt solid #000000; text-align: center; width: 60mm; vertical-align: middle;"><strong>Brances</strong></td>
+                <td rowspan="3" style="border: 1pt solid #000000; text-align: center; width: 25mm; vertical-align: middle;"><strong>M2 <br> (Actual <br> Space WH)</strong></td>
+                <td rowspan="3" style="border: 1pt solid #000000; text-align: center; width: 25mm; vertical-align: middle;"><strong>In CBM</strong></td>
+                <td colspan="7" style="border: 1pt solid #000000; text-align: center;"><strong>ON HAND (M3)</strong></td>
+                <td colspan="7" style="border: 1pt solid #000000; text-align: center;"><strong>INTRANSIT (M3)</strong></td>
+                <td colspan="4" style="border: 1pt solid #000000; text-align: center;"><strong>TOTAL</strong></td>
+              </tr>
+              <tr>
+                <td colspan="5" style="border: 1pt solid #000000; text-align: center;"><strong>CATEGORY</strong></td>
+                <td rowspan="2" style="border: 1pt solid #000000; text-align: center; width: 20mm; vertical-align: middle;"><strong>SUB <br> TOTAL</strong></td>
+                <td rowspan="2" style="border: 1pt solid #000000; text-align: center; width: 20mm; vertical-align: middle;"><strong>%</strong></td>
+                <td colspan="5" style="border: 1pt solid #000000; text-align: center;"><strong>CATEGORY</strong></td>
+                <td rowspan="2" style="border: 1pt solid #000000; text-align: center; width: 20mm; vertical-align: middle;"><strong>SUB <br> TOTAL</strong></td>
+                <td rowspan="2" style="border: 1pt solid #000000; text-align: center; width: 20mm; vertical-align: middle;"><strong>%</strong></td>
+                <td colspan="2" style="border: 1pt solid #000000; text-align: center;"><strong>USAGE C</strong></td>
+                <td colspan="2" style="border: 1pt solid #000000; text-align: center;"><strong>FREE D</strong></td>
+              </tr>
+              <tr>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>REFF</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>WM</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>LCD TV</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>AC</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>OTHERS</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>REFF</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>WM</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>LCD TV</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>AC</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>OTHERS</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>M3</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>%</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>M3</strong></td>
+                <td style="border: 1pt solid #000000; text-align: center; width: 20mm;"><strong>%</strong></td>
+              </tr>
+              <tr>
+                <td style="border: 1pt solid #000000;">10-PT. SEID CAB. SEMARANG</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0</td>
+                <td style="text-align: right; border: 1pt solid #000000;">7464134</td>
+                <td style="text-align: right; border: 1pt solid #000000;">-15345</td>
+                <td style="text-align: right; border: 1pt solid #000000;">1371</td>
+                <td style="text-align: right; border: 1pt solid #000000;">5592</td>
+                <td style="text-align: right; border: 1pt solid #000000;">4154</td>
+                <td style="text-align: right; border: 1pt solid #000000;">7459906</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0%</td>
+                <td style="text-align: right; border: 1pt solid #000000;">101016</td>
+                <td style="text-align: right; border: 1pt solid #000000;">48551</td>
+                <td style="text-align: right; border: 1pt solid #000000;">18082</td>
+                <td style="text-align: right; border: 1pt solid #000000;">35913</td>
+                <td style="text-align: right; border: 1pt solid #000000;">12760</td>
+                <td style="text-align: right; border: 1pt solid #000000;">216322</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0%</td>
+                <td style="text-align: right; border: 1pt solid #000000;">7676228</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0%</td>
+                <td style="text-align: right; border: 1pt solid #000000;">-7676228</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0%</td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0</td>
+                <td style="text-align: right; border: 1pt solid #000000;">7464134</td>
+                <td style="text-align: right; border: 1pt solid #000000;">-15345</td>
+                <td style="text-align: right; border: 1pt solid #000000;">1371</td>
+                <td style="text-align: right; border: 1pt solid #000000;">5592</td>
+                <td style="text-align: right; border: 1pt solid #000000;">4154</td>
+                <td style="text-align: right; border: 1pt solid #000000;">7459906</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0%</td>
+                <td style="text-align: right; border: 1pt solid #000000;">101016</td>
+                <td style="text-align: right; border: 1pt solid #000000;">48551</td>
+                <td style="text-align: right; border: 1pt solid #000000;">18082</td>
+                <td style="text-align: right; border: 1pt solid #000000;">35913</td>
+                <td style="text-align: right; border: 1pt solid #000000;">12760</td>
+                <td style="text-align: right; border: 1pt solid #000000;">216322</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0%</td>
+                <td style="text-align: right; border: 1pt solid #000000;">7676228</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0%</td>
+                <td style="text-align: right; border: 1pt solid #000000;">-7676228</td>
+                <td style="text-align: right; border: 1pt solid #000000;">0%</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
