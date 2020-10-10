e138719347175c2f19d0bc4ae901df2ddddd8857
ker0c0w <azwar.nazz@gmail.com>
2020-10-10 11:15:07 +0700

    rev - select gate user HQ

--- app/Models/Gate.php
+++ app/Models/Gate.php
@@ -20,26 +20,29 @@ class Gate extends BaseModel
 
   public static function getLoadingGate($area)
   {
-    return Gate::leftjoin(
-        DB::raw(
-          '(SELECT
-           wms_pickinglist_header.`gate_number` AS picking_gate_number,
-           wms_pickinglist_header.vehicle_number,
-           IF(wms_pickinglist_header.city_code = "AS", "AS", wms_pickinglist_header.driver_id) AS driver_id,
-           wms_pickinglist_header.`driver_register_id`,
-           log_manifest_header.`do_manifest_no`,
-           wms_pickinglist_header.`city_code`,
-           log_manifest_header.`status_complete`
-         FROM wms_pickinglist_header
-         LEFT JOIN log_manifest_header ON log_manifest_header.`driver_register_id` = wms_pickinglist_header.`driver_register_id`
-         WHERE (log_manifest_header.`status_complete` != 1 OR log_manifest_header.`status_complete` IS NULL)
-         GROUP BY wms_pickinglist_header.`driver_register_id`
-       ) AS t'
-        ),
-        't.picking_gate_number', '=', 'tr_gate.gate_number'
-      )
-      ->where('area', $area)
-      ->orderBy('gate_number')
-    ;
+
+    $query = Gate::leftjoin(
+              DB::raw(
+                '(SELECT
+                wms_pickinglist_header.`gate_number` AS picking_gate_number,
+                wms_pickinglist_header.vehicle_number,
+                IF(wms_pickinglist_header.city_code = "AS", "AS", wms_pickinglist_header.driver_id) AS driver_id,
+                wms_pickinglist_header.`driver_register_id`,
+                log_manifest_header.`do_manifest_no`,
+                wms_pickinglist_header.`city_code`,
+                log_manifest_header.`status_complete`
+              FROM wms_pickinglist_header
+              LEFT JOIN log_manifest_header ON log_manifest_header.`driver_register_id` = wms_pickinglist_header.`driver_register_id`
+              WHERE (log_manifest_header.`status_complete` != 1 OR log_manifest_header.`status_complete` IS NULL)
+              GROUP BY wms_pickinglist_header.`driver_register_id`
+            ) AS t'
+              ),
+              't.picking_gate_number', '=', 'tr_gate.gate_number'
+            )->orderBy('gate_number');
+      if(!auth()->user()->cabang->hq){
+        $query->where('area', $area);
+      }            
+
+    return $query;          
   }
 }
