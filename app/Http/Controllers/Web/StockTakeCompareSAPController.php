7316fd2b0392b306e1c384ca31bbabac18f8291e
ker0c0w <azwar.nazz@gmail.com>
2020-10-10 15:08:18 +0700

    coba fixing stock compare sap

--- app/Http/Controllers/Web/StockTakeCompareSAPController.php
+++ app/Http/Controllers/Web/StockTakeCompareSAPController.php
@@ -15,19 +15,40 @@ class StockTakeCompareSAPController extends Controller
   public function index(Request $request)
   {
     if ($request->ajax()) {
+      
+      // below original query
+      
+      // $query = StockTakeScheduleDetail::select(
+      //   DB::raw('log_stocktake_schedule_detail.qty AS quantitySAP'),
+      //   'log_stocktake_schedule_detail.material_no',
+      //   'log_stocktake_input1.id',
+      //   'log_stocktake_input1.no_tag',
+      //   'log_stocktake_input1.model',
+      //   'log_stocktake_input1.quantity',
+      //   'log_stocktake_input1.location',
+      //   DB::raw('log_stocktake_input2.id AS id2'),
+      //   DB::raw('log_stocktake_input2.no_tag AS no_tag2'),
+      //   DB::raw('log_stocktake_input2.model AS model2'),
+      //   DB::raw('log_stocktake_input2.quantity AS quantity2'),
+      //   DB::raw('log_stocktake_input2.location AS location2')
+      // )
+      //   ->leftjoin('log_stocktake_input1', function ($join) {
+      //     $join->on('log_stocktake_input1.sto_id', '=', 'log_stocktake_schedule_detail.sto_id');
+      //     $join->on('log_stocktake_input1.model', '=', 'log_stocktake_schedule_detail.material_no');
+      //   })
+      //   ->leftjoin('log_stocktake_input2', function ($join) {
+      //     $join->on('log_stocktake_input1.sto_id', '=', 'log_stocktake_input2.sto_id');
+      //     $join->on('log_stocktake_input1.no_tag', '=', 'log_stocktake_input2.no_tag');
+      //   })
+      //   ->where('log_stocktake_schedule_detail.sto_id', $request->input('sto_id'))
+      //   // ->whereRaw('(log_stocktake_input1.input_date IS NOT NULL AND log_stocktake_input2.input_date IS NOT NULL)')
+      // ;
+
       $query = StockTakeScheduleDetail::select(
-        DB::raw('log_stocktake_schedule_detail.qty AS quantitySAP'),
-        'log_stocktake_schedule_detail.material_no',
-        'log_stocktake_input1.id',
-        'log_stocktake_input1.no_tag',
-        'log_stocktake_input1.model',
-        'log_stocktake_input1.quantity',
-        'log_stocktake_input1.location',
-        DB::raw('log_stocktake_input2.id AS id2'),
-        DB::raw('log_stocktake_input2.no_tag AS no_tag2'),
-        DB::raw('log_stocktake_input2.model AS model2'),
-        DB::raw('log_stocktake_input2.quantity AS quantity2'),
-        DB::raw('log_stocktake_input2.location AS location2')
+        DB::raw('sum(log_stocktake_schedule_detail.qty) AS quantitySAP'),
+        'log_stocktake_schedule_detail.material_no',      
+        'sum(log_stocktake_input1.quantity) as quantity',           
+        DB::raw('sum(log_stocktake_input2.quantity) AS quantity2')
       )
         ->leftjoin('log_stocktake_input1', function ($join) {
           $join->on('log_stocktake_input1.sto_id', '=', 'log_stocktake_schedule_detail.sto_id');
@@ -39,7 +60,9 @@ class StockTakeCompareSAPController extends Controller
         })
         ->where('log_stocktake_schedule_detail.sto_id', $request->input('sto_id'))
         // ->whereRaw('(log_stocktake_input1.input_date IS NOT NULL AND log_stocktake_input2.input_date IS NOT NULL)')
+        ->groupBy('log_stocktake_schedule_detail.material_no')
       ;
+
       $datatables = DataTables::of($query)
         ->addIndexColumn() //DT_RowIndex (Penomoran)
         ->addColumn('sap_vs_input_1', function($data){
