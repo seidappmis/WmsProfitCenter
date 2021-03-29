95430ca13ca27b96ded689984bbbfc9b1d1f4d01
Kurnia Wahidin <kurniawahidin@gmail.com>
2021-03-30 06:44:57 +0700

    Urutkan DO di Invoice Receipt

--- app/Models/InvoiceReceiptHeader.php
+++ app/Models/InvoiceReceiptHeader.php
@@ -17,7 +17,7 @@ class InvoiceReceiptHeader extends Model
 
   public function details()
   {
-    return $this->hasMany('App\Models\InvoiceReceiptDetail', 'id_header');
+    return $this->hasMany('App\Models\InvoiceReceiptDetail', 'id_header')->orderBy('delivery_no');
   }
 
   public function getAmountInvoice()
