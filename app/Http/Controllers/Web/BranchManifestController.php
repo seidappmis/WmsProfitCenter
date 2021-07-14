c1bb097c3dd3b789ba13d2536a507256a8b4577e
Kurnia Wahidin <kurniawahidin@gmail.com>
2021-07-14 13:23:50 +0700

    Cek Branch Manifest Double

--- app/Http/Controllers/Web/BranchManifestController.php
+++ app/Http/Controllers/Web/BranchManifestController.php
@@ -92,62 +92,71 @@ class BranchManifestController extends Controller
       'driver_register_id' => 'required',
     ]);
 
-    $manifestHeader = new WMSBranchManifestHeader;
+	$cek = WMSBranchManifestHeader::where([
+			'driver_register_id' => $request->input('driver_register_id'),
+			'manifest_type' => 'NORMAL',
+		])->exists();
+	
+	if($cek){
+		return sendError('Manifest telah ada');
+	}else{
+		$manifestHeader = new WMSBranchManifestHeader;
 
-    // DO MANIFEST NO => KODEAREA-ymd-URUT
-    $prefix = auth()->user()->cabang->short_description . '-' . date('ymd');
+		// DO MANIFEST NO => KODEAREA-ymd-URUT
+		$prefix = auth()->user()->cabang->short_description . '-' . date('ymd');
 
-    $prefix_length = strlen($prefix);
-    $max_no        = DB::select('SELECT MAX(SUBSTR(do_manifest_no, ?)) AS max_no FROM wms_branch_manifest_header WHERE SUBSTR(do_manifest_no,1,?) = ? ', [$prefix_length + 2, $prefix_length, $prefix])[0]->max_no;
-    $max_no        = str_pad($max_no + 1, 3, 0, STR_PAD_LEFT);
+		$prefix_length = strlen($prefix);
+		$max_no        = DB::select('SELECT MAX(SUBSTR(do_manifest_no, ?)) AS max_no FROM wms_branch_manifest_header WHERE SUBSTR(do_manifest_no,1,?) = ? ', [$prefix_length + 2, $prefix_length, $prefix])[0]->max_no;
+		$max_no        = str_pad($max_no + 1, 3, 0, STR_PAD_LEFT);
 
-    $do_manifest_no = $prefix . '-' . $max_no;
+		$do_manifest_no = $prefix . '-' . $max_no;
 
-    $manifestHeader->driver_register_id          = $request->input('driver_register_id');
-    $manifestHeader->do_manifest_no              = $do_manifest_no;
-    $manifestHeader->expedition_code             = $request->input('expedition_code');
-    $manifestHeader->expedition_name             = $request->input('expedition_name');
-    $manifestHeader->driver_id                   = $request->input('driver_id');
-    $manifestHeader->driver_name                 = $request->input('driver_name');
-    $manifestHeader->vehicle_number              = $request->input('vehicle_number');
-    $manifestHeader->vehicle_code_type           = $request->input('vehicle_code_type');
-    $manifestHeader->vehicle_description         = $request->input('vehicle_description');
-    $manifestHeader->do_manifest_date            = $request->input('do_manifest_date');
-    $manifestHeader->do_manifest_time            = date('Y-m-d H:i:s');
-    $manifestHeader->destination_number_driver   = $request->input('destination_number_driver');
-    $manifestHeader->destination_name_driver     = $request->input('destination_name_driver');
-    $manifestHeader->city_code                   = $request->input('city_code');
-    $manifestHeader->city_name                   = $request->input('city_name');
-    $manifestHeader->container_no                = $request->input('container_no');
-    $manifestHeader->seal_no                     = $request->input('seal_no');
-    $manifestHeader->checker                     = $request->input('checker');
-    $manifestHeader->pdo_no                      = $request->input('pdo_no');
-    $manifestHeader->kode_cabang                 = auth()->user()->cabang->kode_cabang;
-    $manifestHeader->status_complete             = 0;
-    $manifestHeader->urut_manifest               = 1;
-    $manifestHeader->tcs                         = 0;
-    $manifestHeader->ambil_sendiri               = 0;
-    $manifestHeader->id_freight_cost             = $request->input('id_freight_cost');
-    $manifestHeader->ritase                      = 0;
-    $manifestHeader->cbm                         = 0;
-    $manifestHeader->manifest_return             = 0;
-    $manifestHeader->manifest_type               = 'NORMAL';
-    $manifestHeader->status_inv_recieve          = 0;
-    $manifestHeader->have_lcl                    = 0;
-    $manifestHeader->lcl_from_driver_register_id = $request->input('lcl_from_driver_register_id');
-    $manifestHeader->lcl_from_manifest_no        = $request->input('lcl_from_manifest_no');
-    $manifestHeader->lcl_created_date            = $request->input('lcl_created_date');
-    $manifestHeader->lcl_created_by              = $request->input('lcl_created_by');
-    $manifestHeader->have_resend                 = 0;
-    $manifestHeader->manifest_resend             = 0;
-    $manifestHeader->r_from_manifest             = $request->input('r_from_manifest');
-    $manifestHeader->r_driver_register_id        = $request->input('r_driver_register_id');
-    $manifestHeader->r_created_date              = $request->input('r_create_date');
-    $manifestHeader->r_created_by                = $request->input('r_create_by');
+		$manifestHeader->driver_register_id          = $request->input('driver_register_id');
+		$manifestHeader->do_manifest_no              = $do_manifest_no;
+		$manifestHeader->expedition_code             = $request->input('expedition_code');
+		$manifestHeader->expedition_name             = $request->input('expedition_name');
+		$manifestHeader->driver_id                   = $request->input('driver_id');
+		$manifestHeader->driver_name                 = $request->input('driver_name');
+		$manifestHeader->vehicle_number              = $request->input('vehicle_number');
+		$manifestHeader->vehicle_code_type           = $request->input('vehicle_code_type');
+		$manifestHeader->vehicle_description         = $request->input('vehicle_description');
+		$manifestHeader->do_manifest_date            = $request->input('do_manifest_date');
+		$manifestHeader->do_manifest_time            = date('Y-m-d H:i:s');
+		$manifestHeader->destination_number_driver   = $request->input('destination_number_driver');
+		$manifestHeader->destination_name_driver     = $request->input('destination_name_driver');
+		$manifestHeader->city_code                   = $request->input('city_code');
+		$manifestHeader->city_name                   = $request->input('city_name');
+		$manifestHeader->container_no                = $request->input('container_no');
+		$manifestHeader->seal_no                     = $request->input('seal_no');
+		$manifestHeader->checker                     = $request->input('checker');
+		$manifestHeader->pdo_no                      = $request->input('pdo_no');
+		$manifestHeader->kode_cabang                 = auth()->user()->cabang->kode_cabang;
+		$manifestHeader->status_complete             = 0;
+		$manifestHeader->urut_manifest               = 1;
+		$manifestHeader->tcs                         = 0;
+		$manifestHeader->ambil_sendiri               = 0;
+		$manifestHeader->id_freight_cost             = $request->input('id_freight_cost');
+		$manifestHeader->ritase                      = 0;
+		$manifestHeader->cbm                         = 0;
+		$manifestHeader->manifest_return             = 0;
+		$manifestHeader->manifest_type               = 'NORMAL';
+		$manifestHeader->status_inv_recieve          = 0;
+		$manifestHeader->have_lcl                    = 0;
+		$manifestHeader->lcl_from_driver_register_id = $request->input('lcl_from_driver_register_id');
+		$manifestHeader->lcl_from_manifest_no        = $request->input('lcl_from_manifest_no');
+		$manifestHeader->lcl_created_date            = $request->input('lcl_created_date');
+		$manifestHeader->lcl_created_by              = $request->input('lcl_created_by');
+		$manifestHeader->have_resend                 = 0;
+		$manifestHeader->manifest_resend             = 0;
+		$manifestHeader->r_from_manifest             = $request->input('r_from_manifest');
+		$manifestHeader->r_driver_register_id        = $request->input('r_driver_register_id');
+		$manifestHeader->r_created_date              = $request->input('r_create_date');
+		$manifestHeader->r_created_by                = $request->input('r_create_by');
 
-    $manifestHeader->save();
+		$manifestHeader->save();
 
-    return $manifestHeader;
+		return $manifestHeader;
+	}
   }
 
   public function update(Request $request, $id)
