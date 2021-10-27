84c43576522cee815c53729bbd2923db07fc8dae
Kurnia Wahidin <kurniawahidin@gmail.com>
2021-10-27 10:10:17 +0700

    Export to excel Marine Cargo

--- resources/views/web/during/marine-cargo/print-notice-of-claim-excel.blade.php
+++ resources/views/web/during/marine-cargo/print-notice-of-claim-excel.blade.php
@@ -3,7 +3,7 @@
   <link rel="stylesheet" type="text/css" href="{{ url('materialize/css/custom/print1-hq.css') }}">
 </head>
 <body style="font-family: courier New; font-size: 9pt;">
-  <table style="font-family: Arial;border-collapse: collapse;" width="100%">
+  <table style="font-family: Arial;border-collapse: collapse;">
    <tr><td colspan="5">&nbsp;</td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
@@ -33,19 +33,19 @@
    <tr><td colspan="5">&nbsp;</td></tr>
    <tr><td colspan="5">The carton box was torn and dented, when the goods still in the container with the details as below:</td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
-   <tr><td colspan="2" style="width: 20%;">Name of Vessel</td><td style="width: 15px;">:</td><td>{{ $marineCargo->vessel_name }}</td><td></td></tr>
+   <tr><td colspan="2">Name of Vessel</td><td style="width: 15px;">:</td><td>{{ $marineCargo->vessel_name }}</td><td></td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
-   <tr><td colspan="2" style="width: 20%;">Sailed On, {{ date('M d, Y', strtotime($marineCargo->sailed_date)) }}</td><td style="width: 15px;">:</td><td>{{ $marineCargo->sailed_on }}</td><td></td></tr>
+   <tr><td colspan="2">Sailed On, {{ date('M d, Y', strtotime($marineCargo->sailed_date)) }}</td><td style="width: 15px;">:</td><td>{{ $marineCargo->sailed_on }}</td><td></td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
-   <tr><td colspan="2" style="width: 20%;">Eta, Tanjung Priuk, Jakarta</td><td style="width: 15px;">:</td><td>{{ date('F d, Y', strtotime($marineCargo->arrived_date)) }}</td><td></td></tr>
+   <tr><td colspan="2">Eta, Tanjung Priuk, Jakarta</td><td style="width: 15px;">:</td><td>{{ date('F d, Y', strtotime($marineCargo->arrived_date)) }}</td><td></td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
-   <tr><td colspan="2" style="width: 20%;">Receiving goods at our Warehouse</td><td style="width: 15px;">:</td><td>{{ date('F d, Y', strtotime($marineCargo->discharging_date)) }}</td><td></td></tr>
+   <tr><td colspan="2">Receiving goods at our Warehouse</td><td style="width: 15px;">:</td><td>{{ date('F d, Y', strtotime($marineCargo->discharging_date)) }}</td><td></td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
-   <tr><td colspan="2" style="width: 20%;">B/L No</td><td style="width: 15px;">:</td><td>{{ $marineCargo->getBLNo() }}</td><td></td></tr>
+   <tr><td colspan="2">B/L No</td><td style="width: 15px;">:</td><td>{{ $marineCargo->getBLNo() }}</td><td></td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
-   <tr><td colspan="2" style="width: 20%;">Invoice No</td><td style="width: 15px;">:</td><td>{{ $marineCargo->getInvoiceNo() }}</td><td></td></tr>
+   <tr><td colspan="2">Invoice No</td><td style="width: 15px;">:</td><td>{{ $marineCargo->getInvoiceNo() }}</td><td></td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
-   <tr><td colspan="2" style="width: 20%;">Nature of Claim</td><td style="width: 15px;">:</td><td>{{ $marineCargo->getNatureOfClaim() }}</td><td></td></tr>
+   <tr><td colspan="2">Nature of Claim</td><td style="width: 15px;">:</td><td>{{ $marineCargo->getNatureOfClaim() }}</td><td></td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
    <tr><td colspan="5">You are kindly to inform us in writing your opinion of this matter on your investigation at an early date.</td></tr>
    <tr><td colspan="5">&nbsp;</td></tr>
